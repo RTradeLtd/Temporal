b249c1adc62bf7af06426e659cfb659474c5dbf6
Alex <solidity@pop-os.localdomain>
2018-05-04 18:06:21 -0700

    added cluster daemon mode to manager script

--- scripts/ipfs_manager_script.sh
+++ scripts/ipfs_manager_script.sh
@@ -9,6 +9,10 @@ MODE="daemon"
 case "$MODE" in
 
     daemon)
-        sudo ipfs daemon 2>&1 | tee --append ipfs.log
+        sudo ipfs daemon 2>&1 | tee --append /var/log/ipfs/ipfs_daemon.log
+        ;;
+
+    cluster-daemon)
+        ipfs-cluster-service daemon --debug | tee --append /var/log/ipfs/ipfs_cluster_daemon.log
         ;;
 esac
