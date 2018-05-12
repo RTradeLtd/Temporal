d3c60e97083dd3481173d19826448894bb5553c4
potsables <potsables@notarealemail>
2018-05-12 00:36:09 -0700

    added basic queue

--- queue/queue.go
+++ queue/queue.go
@@ -1,6 +1,8 @@
 package queue
 
 import (
+	"log"
+
 	"github.com/streadway/amqp"
 )
 
@@ -9,6 +11,8 @@ var ipfsClusterQueue = "ipfs-cluster"
 
 type QueueManager struct {
 	Connection *amqp.Connection
+	Channel    *amqp.Channel
+	Queue      *amqp.Queue
 }
 
 func Setup() (*amqp.Connection, error) {
@@ -19,10 +23,46 @@ func Setup() (*amqp.Connection, error) {
 	return conn, nil
 }
 
-func (qm *QueueManager) Close() {
-	qm.Connection.Close()
+func (qm *QueueManager) OpenChannel() {
+	ch, err := qm.Connection.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	qm.Channel = ch
 }
 
-func (qm *QueueManager) DeclareQueue(queueName string) {
+func (qm *QueueManager) DeclareQueue() {
+	q, err := qm.Channel.QueueDeclare(
+		"name", // name
+		false,  // durable
+		false,  // delete when unused
+		false,  // exclusive
+		false,  // no-wait
+		nil,    // arguments
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	qm.Queue = &q
+}
 
+func (qm *QueueManager) PublishMessage(msg string) {
+	body := msg
+	err := qm.Channel.Publish(
+		"",            // exchange
+		qm.Queue.Name, // routing key
+		false,         // mandatory
+		false,         //immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (qm *QueueManager) Close() {
+	qm.Connection.Close()
 }
