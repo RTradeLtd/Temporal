39c407b8e72bcf3a836d6640bd31f76e422e62e0
potsables <alextrottier2014@gmail.com>
2018-05-15 19:48:55 -0700

    Moving functions to appropriate models
    After moving the functions over to all the models, I'll implement the rabbitmq backend worker that is responsible for storing messages in the database. We will transition to a system where after sending off the data to rabbitmq to store in the database, we begin adding the data to ipfs

--- database/database.go
+++ database/database.go
@@ -1,14 +1,9 @@
 package database
 
 import (
-	"errors"
-	"fmt"
 	"log"
-	"net/http"
-	"strconv"
 
 	"github.com/RTradeLtd/Temporal/models"
-	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
@@ -42,7 +37,8 @@ type DatabaseManager struct {
 
 func Initialize() *DatabaseManager {
 	dbm := DatabaseManager{}
-	dbm.OpenDBConnection()
+	db := OpenDBConnection()
+	dbm.DB = db
 	dbm.RunMigrations()
 	return &dbm
 }
@@ -53,83 +49,15 @@ func (dbm *DatabaseManager) RunMigrations() {
 }
 
 // OpenDBConnection is used to create a database connection
-func (dbm *DatabaseManager) OpenDBConnection() {
+func OpenDBConnection() *gorm.DB {
 	db, err := gorm.Open("sqlite3", "./ipfs_database.db")
 	if err != nil {
 		log.Fatal(err)
 	}
-	dbm.DB = db
+	return db
 }
 
 // CloseDBConnection is used to close a db
 func CloseDBConnection(db *gorm.DB) {
-	db.Close()
-}
-
-func (dbm *DatabaseManager) GetUpload(hash string, uploaderAddress string) []*models.Upload {
-	var uploads []*models.Upload
-	dbm.DB.Find(&uploads).Where("hash = ? AND uploader_address = ?", hash, uploaderAddress)
-	return uploads
-}
-
-// GetUploads is used to retrieve all uploads
-func GetUploads() []*models.Upload {
-	var uploads []*models.Upload
-	db = OpenDBConnection()
-	db.Find(&uploads)
-	return uploads
-}
-
-func GetUploadsForAddress(address string) []*models.Upload {
-	var uploads []*models.Upload
-	db = OpenDBConnection()
-	db.Where("upload_address = ?", address).Find(&uploads)
-	return uploads
-}
-
-// AddHash his used to add a hash to our database
-func AddHash(c *gin.Context) error {
-	var upload models.Upload
-	hash := c.Param("hash")
-	address, exists := c.GetPostForm("uploadAddress")
-	if !exists {
-		c.AbortWithError(http.StatusBadRequest, errors.New("uploadAddress param des not exist"))
-		return errors.New("uploadAddress param des not exist")
-	}
-	holdTime, exists := c.GetPostForm("holdTime")
-	if !exists {
-		c.AbortWithError(http.StatusBadRequest, errors.New("holdTime param does not exist"))
-		return errors.New("holdTime param does not exist")
-	}
-	holdTimeInt, err := strconv.ParseInt(holdTime, 10, 64)
-	if err != nil {
-		c.AbortWithError(http.StatusBadRequest, err)
-		return err
-	}
-	upload.Hash = fmt.Sprintf("%s", hash)
-	upload.Type = "pin"
-	upload.HoldTimeInMonths = holdTimeInt
-	upload.UploadAddress = address
-	db := OpenDBConnection()
-	db.Create(&upload)
-	db.Close()
-	return nil
-}
-
-// AddFileHash is used to add the hash of a file to our database
-func AddFileHash(c *gin.Context, hash string) {
-	var upload models.Upload
-	address := c.PostForm("uploadAddress")
-	holdTimeInt, err := strconv.ParseInt(c.PostForm("holdTime"), 10, 64)
-	if err != nil {
-		c.Error(err)
-	}
-	upload.HoldTimeInMonths = holdTimeInt
-	upload.UploadAddress = address
-	upload.Hash = hash
-	upload.Type = "file"
-	db := OpenDBConnection()
-	db.AutoMigrate(&upload)
-	db.Create(&upload)
 	db.Close()
 }
--- models/upload.go
+++ models/upload.go
@@ -1,6 +1,13 @@
 package models
 
 import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 )
 
@@ -51,4 +58,60 @@ func (um *UploadManager) GetUploadsForAddress(address string) []*Upload {
 	var uploads []*Upload
 	um.DB.Where("upload_address = ?", address).Find(&uploads)
 	return uploads
+}
+
+// AddHash his used to add a hash to our database
+func AddHash(c *gin.Context) error {
+	var upload Upload
+	hash := c.Param("hash")
+	address, exists := c.GetPostForm("uploadAddress")
+	if !exists {
+		c.AbortWithError(http.StatusBadRequest, errors.New("uploadAddress param des not exist"))
+		return errors.New("holdTime param does not exist")
+	}
+	holdTime, exists := c.GetPostForm("holdTime")
+	if !exists {
+		c.AbortWithError(http.StatusBadRequest, errors.New("holdTime param does not exist"))
+		return errors.New("holdTime param does not exist")
+	}
+	holdTimeInt, err := strconv.ParseInt(holdTime, 10, 64)
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return err
+	}
+	upload.Hash = fmt.Sprintf("%s", hash)
+	upload.Type = "pin"
+	upload.HoldTimeInMonths = holdTimeInt
+	upload.UploadAddress = address
+	db := OpenDBConnection()
+	db.Create(&upload)
+	db.Close()
+	return nil
+}
+
+// AddFileHash is used to add the hash of a file to our database
+func AddFileHash(c *gin.Context, hash string) {
+	var upload Upload
+	address := c.PostForm("uploadAddress")
+	holdTimeInt, err := strconv.ParseInt(c.PostForm("holdTime"), 10, 64)
+	if err != nil {
+		c.Error(err)
+	}
+	upload.HoldTimeInMonths = holdTimeInt
+	upload.UploadAddress = address
+	upload.Hash = hash
+	upload.Type = "file"
+	db := OpenDBConnection()
+	db.AutoMigrate(&upload)
+	db.Create(&upload)
+	db.Close()
+}
+
+// OpenDBConnection is used to open a database connection
+func OpenDBConnection() *gorm.DB {
+	db, err := gorm.Open("sqlite3", "./ipfs_database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
 }
