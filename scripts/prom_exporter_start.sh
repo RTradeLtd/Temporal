6eea75854dda488ac616205de8f9fba878b67c84
potsables <alextrottier2014@gmail.com>
2018-05-19 17:45:01 -0700

    added node exporter scripts

---
+++ scripts/prom_exporter_start.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# used to start our prometheus node exporter
+/usr/local/bin/prom_node_exporter/node_exporter
--- scripts/prom_node_exporter_install.sh
+++ scripts/prom_node_exporter_install.sh
@@ -7,4 +7,6 @@ wget https://github.com/prometheus/node_exporter/releases/download/v0.16.0/node_
 tar zxvf node_exporter*.tar.gz
 rm *.gz
 mkdir /usr/local/bin/prom_node_exporter
-mv node_exporter* /usr/local/bin/prom_node_exporter
+mv ~/node_exporter*/* /usr/local/bin/prom_node_exporter
+cp ~/Temporal/scripts/prom_exporter_start.sh /boot_scripts/prom_exporter_start.sh
+chmod a+x /boot_scripts/prom_exporter_start.sh
