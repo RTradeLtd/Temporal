918498f16bf4d913c56ce95481206d8f3c3bd29f
potsables <alextrottier2014@gmail.com>
2018-05-19 18:37:28 -0700

    start script wasn't enabling pubsub

--- scripts/ipfs_manager_script.sh
+++ scripts/ipfs_manager_script.sh
@@ -10,7 +10,7 @@ export IPFS_PATH=/srv/dev-disk-by-label-Data/ipfs-data
 case "$MODE" in
 
     daemon)
-        ipfs daemon 2>&1 | tee --append /var/log/ipfs/ipfs_daemon.log
+        ipfs daemon --enable-pubsub-experiment 2>&1 | tee --append /var/log/ipfs/ipfs_daemon.log
         ;;
 
     cluster-daemon)
