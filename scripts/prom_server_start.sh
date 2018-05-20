039b28c2de6816a88eb183f9bcc854d30b0523ac
potsables <alextrottier2014@gmail.com>
2018-05-19 17:32:39 -0700

    updated script

--- scripts/prom_server_start.sh
+++ scripts/prom_server_start.sh
@@ -3,4 +3,4 @@
 # used to start our prometheus server
 
 CONFIG_FILE="/usr/local/bin/prometheus_server/prometheus.yml"
-/usr/local/bin/prometheus_server/prometheus "$CONFIG_FILE"
+/usr/local/bin/prometheus_server/prometheus --config.file="$CONFIG_FILE"
