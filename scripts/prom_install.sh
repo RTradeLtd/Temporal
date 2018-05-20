227d0a1d76facd458ab328c4ac2fb01f1abb24e7
potsables <alextrottier2014@gmail.com>
2018-05-19 17:25:48 -0700

    small update to prometheus installl

--- scripts/prom_install.sh
+++ scripts/prom_install.sh
@@ -6,4 +6,5 @@ wget https://github.com/prometheus/prometheus/releases/download/v2.2.1/prometheu
 tar zxvf prometheus-*tar.gz
 rm *.gz
 mkdir /usr/local/bin/prometheus_server
+mv ~/prometheus-* /usr/local/bin/prometheus_server
 cp ~/Temporal/configs/prometheus.yml /usr/local/bin/prometheus_server
