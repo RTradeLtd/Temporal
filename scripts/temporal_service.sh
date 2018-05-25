e6b17d7cec90f0b8885aeb42782e0ff2f497766a
potsables <alextrottier2014@gmail.com>
2018-05-24 19:26:37 -0700

    another typo

--- scripts/temporal_service.sh
+++ scripts/temporal_service.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # used to launch temporal services
-/boot_scripts/temporal_manager api &
-/boot_scripts/temporal_manager queue-dpa &
-/boot_scripts/temporal_manager queue-dfa &
-/boot_scripts/temporal_manager ipfs-cluster-queue &
+/boot_scripts/temporal_manager.sh api &
+/boot_scripts/temporal_manager.sh queue-dpa &
+/boot_scripts/temporal_manager.sh queue-dfa &
+/boot_scripts/temporal_manager.sh ipfs-cluster-queue &
