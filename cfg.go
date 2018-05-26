9306372682dc62d2ba366f97caf2f6b92323f79f
potsables <alextrottier2014@gmail.com>
2018-05-26 00:50:24 -0700

    added the example config file being used

--- cfg.go
+++ cfg.go
@@ -27,6 +27,22 @@ type TemporalConfig struct {
 			ListenAddress string `json:"listen_address"`
 		} `json:"connection"`
 	} `json:"api"`
+	Ethereum struct {
+		Account struct {
+			Address string `json:"address"`
+			KeyFile string `json:"key_file"`
+			KeyPass string `json:"key_pass"`
+		} `json:"account"`
+		Connection struct {
+			RPC struct {
+				IP   string `json:"ip"`
+				Port string `json:"port"`
+			} `json:"rpc"`
+			IPC struct {
+				Path string `json:"path"`
+			} `json:"ipc"`
+		} `json:"connection"`
+	} `json:"ethereum"`
 }
 
 func config(cfgCid string) {
