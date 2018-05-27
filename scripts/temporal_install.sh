399e476a56ea02d00de72bb587b5901e8095bb6d
potsables <alextrottier2014@gmail.com>
2018-05-27 13:36:07 -0700

    reintroduced prom node installer

--- scripts/temporal_install.sh
+++ scripts/temporal_install.sh
@@ -7,7 +7,7 @@ sudo mkdir -p /ipfs/ipfs-cluster
 sudo mkdir -p /boot_scripts
 sudo bash -x ~/go/src/github.com/RTradeLtd/Temporal/scripts/ipfs_install.sh
 sudo bash -x ~/go/src/github.com/RTradeLtd/Temporal/scripts/cluster_install.sh
-# this is currently broken sudo bash -x ~/go/src/github.com/RTradeLtd/Temporal/scripts/prom_node_exporter_install.sh
+sudo bash -x ~/go/src/github.com/RTradeLtd/Temporal/scripts/prom_node_exporter_install.sh
 sudo cp ~/go/src/github.com/RTradeLtd/Temporal/scripts/temporal_manager.sh /boot_scripts
 sudo cp ~/go/src/github.com/RTradeLtd/Temporal/scripts/cluster_manager.sh /boot_scripts
 sudo cp ~/go/src/github.com/RTradeLtd/Temporal/scripts/ipfs_manager_script.sh /boot_scripts
