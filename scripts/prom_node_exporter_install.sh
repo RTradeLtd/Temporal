89d4fa6845f5f4bc888713f325d6c9164a843806
potsables <alextrottier2014@gmail.com>
2018-05-27 12:12:06 -0700

    fixed prom node exporter install

--- scripts/prom_node_exporter_install.sh
+++ scripts/prom_node_exporter_install.sh
@@ -8,7 +8,7 @@ tar zxvf node_exporter*.tar.gz
 rm *.gz
 mkdir /usr/local/bin/prom_node_exporter
 mv ~/node_exporter*/* /usr/local/bin/prom_node_exporter
-cp ~/Temporal/scripts/prom_exporter_start.sh /boot_scripts/prom_exporter_start.sh
+cp ~/go/src/github.com/RTradeLtd/Temporal/scripts/prom_exporter_start.sh /boot_scripts/prom_exporter_start.sh
 chmod a+x /boot_scripts/prom_exporter_start.sh
-cp ~/Temporal/configs/prom_node_exporter.service /etc/systemd/system
+cp ~/go/src/github.com/RTradeLtd/Temporal/configs/prom_node_exporter.service /etc/systemd/system
 systemctl enable prom_node_exporter.service
