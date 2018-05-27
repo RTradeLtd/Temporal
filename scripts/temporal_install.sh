9ac0c35180a56a432e0a83c7e99f8295774d1552
potsables <alextrottier2014@gmail.com>
2018-05-26 21:22:46 -0700

    typo

--- scripts/temporal_install.sh
+++ scripts/temporal_install.sh
@@ -5,9 +5,9 @@ mkdir -p /var/log/ipfs
 mkdir -p /ipfs
 kdir -p /ipfs/ipfs-cluster
 mkdir -p /boot_scripts
-bash -x ~/go/src/github.com/RTradeLtd/scripts/ipfs_install.sh
-bash -x ~/go/src/github.com/RTradeLtd/scripts/cluster_install.sh
-bash -x ~/go/src/github.com/RTradeLtd/scripts/prom_node_exporter_install.sh
-cp ~/go/src/github.com/RTradeLtd/scripts/temporal_manager.sh /boot_scripts
-cp ~/go/src/github.com/RTradeLtd/scripts/cluster_manager.sh /boot_scripts
-cp ~/go/src/github.com/RTradeLtd/scripts/ipfs_manager_script.sh /boot_scripts
+bash -x ~/go/src/github.com/RTradeLtd/Temporal/scripts/ipfs_install.sh
+bash -x ~/go/src/github.com/RTradeLtd/Temporal/scripts/cluster_install.sh
+bash -x ~/go/src/github.com/RTradeLtd/Temporal/scripts/prom_node_exporter_install.sh
+cp ~/go/src/github.com/RTradeLtd/Temporal/scripts/temporal_manager.sh /boot_scripts
+cp ~/go/src/github.com/RTradeLtd/Temporal/scripts/cluster_manager.sh /boot_scripts
+cp ~/go/src/github.com/RTradeLtd/Temporal/scripts/ipfs_manager_script.sh /boot_scripts
