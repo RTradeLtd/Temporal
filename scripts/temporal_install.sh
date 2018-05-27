804069b6170d75c33e960a8b02d8385bac725380
potsables <alextrottier2014@gmail.com>
2018-05-26 21:24:52 -0700

    added sudo

--- scripts/temporal_install.sh
+++ scripts/temporal_install.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-mkdir -p /var/log/temporal
-mkdir -p /var/log/ipfs
-mkdir -p /ipfs
-kdir -p /ipfs/ipfs-cluster
-mkdir -p /boot_scripts
-bash -x ~/go/src/github.com/RTradeLtd/Temporal/scripts/ipfs_install.sh
-bash -x ~/go/src/github.com/RTradeLtd/Temporal/scripts/cluster_install.sh
-bash -x ~/go/src/github.com/RTradeLtd/Temporal/scripts/prom_node_exporter_install.sh
-cp ~/go/src/github.com/RTradeLtd/Temporal/scripts/temporal_manager.sh /boot_scripts
-cp ~/go/src/github.com/RTradeLtd/Temporal/scripts/cluster_manager.sh /boot_scripts
-cp ~/go/src/github.com/RTradeLtd/Temporal/scripts/ipfs_manager_script.sh /boot_scripts
+sudo mkdir -p /var/log/temporal
+sudo mkdir -p /var/log/ipfs
+sudo mkdir -p /ipfs
+sudo mkdir -p /ipfs/ipfs-cluster
+sudo mkdir -p /boot_scripts
+sudo bash -x ~/go/src/github.com/RTradeLtd/Temporal/scripts/ipfs_install.sh
+sudo bash -x ~/go/src/github.com/RTradeLtd/Temporal/scripts/cluster_install.sh
+sudo bash -x ~/go/src/github.com/RTradeLtd/Temporal/scripts/prom_node_exporter_install.sh
+sudo cp ~/go/src/github.com/RTradeLtd/Temporal/scripts/temporal_manager.sh /boot_scripts
+sudo cp ~/go/src/github.com/RTradeLtd/Temporal/scripts/cluster_manager.sh /boot_scripts
+sudo cp ~/go/src/github.com/RTradeLtd/Temporal/scripts/ipfs_manager_script.sh /boot_scripts
