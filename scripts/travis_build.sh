2dd19bd950e2864e2cbad904c3b7c94958499048
potsables <alextrottier2014@gmail.com>
2018-05-27 23:21:25 -0700

    attempting to install ipfs onto trafics

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
-echo "helelelelelelelelelel"
-echo "helelelelelelelelelel"
-echo "helelelelelelelelelel"
-echo "helelelelelelelelelel"
-echo "helelelelelelelelelel"
-echo "helelelelelelelelelel"
-echo "helelelelelelelelelel"
-echo "helelelelelelelelelel"
-echo "helelelelelelelelelel"
+# download go ipfs
+echo "[INFO] Downloading IPFS"
+wget https://dist.ipfs.io/go-ipfs/v0.4.15/go-ipfs_v0.4.15_linux-amd64.tar.gz
+tar zxvf go-ipfs*.gz
+rm *gz
+cd go-ipfs
+echo "[INFO] Installing IPFS"
+sudo ./install.sh
+which ipfs
+if [[ "$?" -ne 0 ]]; then
+    echo "[ERROR] IPFS Installation Failed"
+    exit 1
+fi
+ipfs init 
