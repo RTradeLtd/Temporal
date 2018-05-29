93feb92cc1c90a437cffe5ac40548371968b2cfb
potsables <alextrottier2014@gmail.com>
2018-05-28 19:39:29 -0700

    updating travis build file

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
 # download go ipfs
+export IPFS_PATH=/ipfs
+
+echo "[INFO] Creating IPFS Repo Directory"
+sudo mkdir -p /ipfs/ipfs-cluster
+
 echo "[INFO] Downloading IPFS"
 wget https://dist.ipfs.io/go-ipfs/v0.4.15/go-ipfs_v0.4.15_linux-amd64.tar.gz
 tar zxvf go-ipfs*.gz
@@ -13,4 +18,6 @@ if [[ "$?" -ne 0 ]]; then
     echo "[ERROR] IPFS Installation Failed"
     exit 1
 fi
-ipfs init 
+
+ipfs init --profile=server,badgerds
+ipfs daemon &
