9ed014e45c9f9f2dd247fdad132f139b366974da
potsables <alextrottier2014@gmail.com>
2018-05-28 22:08:10 -0700

    adding more tests. travis doesn't like this build for some reason will debug later

--- rtfs/rtfs_test.go
+++ rtfs/rtfs_test.go
@@ -11,14 +11,14 @@ const testPIN = "QmNZiPk974vDsPmQii3YbrMKfi12KTSNM7XMiYyiea4VYZ"
 
 func TestInitialize(t *testing.T) {
 	im := rtfs.Initialize("")
-	_, err := im.Shell.ID()
+	info, err := im.Shell.ID()
 	if err != nil {
 		fmt.Println(err)
 		t.Fatal(err)
 	}
+	fmt.Println(info)
 }
 
-/*
 func TestPin(t *testing.T) {
 	im := rtfs.Initialize("")
 	err := im.Pin(testPIN)
@@ -26,7 +26,7 @@ func TestPin(t *testing.T) {
 		t.Fatal(err)
 	}
 	fmt.Println("pin test successful")
-}*/
+}
 
 func TestGetObjectFileSizeInBytes(t *testing.T) {
 	im := rtfs.Initialize("")
@@ -35,4 +35,24 @@ func TestGetObjectFileSizeInBytes(t *testing.T) {
 		t.Fatal(err)
 	}
 	fmt.Println("file size in bytes ", size)
+}
+
+func TestObjectStat(t *testing.T) {
+	im := rtfs.Initialize("")
+	stat, err := im.ObjectStat(testPIN)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("object stat ", stat)
+}
+
+func TestParseLocalPinsForHash(t *testing.T) {
+	im := rtfs.Initialize("")
+	exists, err := im.ParseLocalPinsForHash(testPIN)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal(err)
+	}
 }
