02945e8607f63b41feda04adc029480875f4633b
potsables <alextrottier2014@gmail.com>
2018-05-28 21:40:11 -0700

    aded sudo

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -4,7 +4,7 @@
 export IPFS_PATH=/ipfs
 
 echo "[INFO] Creating IPFS Repo Directory"
-mkdir -p /ipfs/ipfs-cluster
+sudo mkdir -p /ipfs/ipfs-cluster
 
 echo "[INFO] Downloading IPFS"
 wget https://dist.ipfs.io/go-ipfs/v0.4.15/go-ipfs_v0.4.15_linux-amd64.tar.gz
@@ -12,7 +12,7 @@ tar zxvf go-ipfs*.gz
 rm *gz
 cd go-ipfs
 echo "[INFO] Installing IPFS"
-./install.sh
+sudo ./install.sh
 which ipfs
 if [[ "$?" -ne 0 ]]; then
     echo "[ERROR] IPFS Installation Failed"
