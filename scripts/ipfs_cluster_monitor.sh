5dd5810ffafa4490ba9d43734dcc1ffc2551b5c2
potsables <alextrottier2014@gmail.com>
2018-06-07 22:16:07 -0700

    added basic script to monitor peer count for ipfs cluster

---
+++ scripts/ipfs_cluster_monitor.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+case "$1" in 
+
+    peer-count)
+        COUNT=$(ipfs-cluster-ctl peers ls | grep "Qm.* |" | grep -v $(hostname) | grep -vi "error" | wc -l)
+        if [[ "$COUNT" -lt 1 ]]; then
+            # if there is no peer, then we print a 1, indicating an error, peers in an error state count as no peer
+            echo "1"
+            exit 1
+        else
+            echo "0"
+            exit 0
+        fi
+        ;;
+    pin-count)
+        COUNT=$(ipfs-cluster-ctl pin ls | wc -l)
+        echo "$COUNT"
+        ;;
+esac
