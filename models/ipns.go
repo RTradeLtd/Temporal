57fc209425d8560c5c2aba7d4ed1602c428c45fb
potsables <alextrottier2014@gmail.com>
2018-06-17 02:04:41 -0700

    building out the ipns backend

--- models/ipns.go
+++ models/ipns.go
@@ -11,6 +11,7 @@ import (
 type IPNS struct {
 	gorm.Model
 	ExpiryDateUnix  int64    `gorm:"type:integer" json:"expiry_date_unix"`
+	Sequence        int64    `gorm:"type:integer;not null;default:0" json:"sequence"`
 	IPNSHash        string   `gorm:"type:varchar(255);unique;column:ipns_hash" json:"ipns_hash"`
 	IPFSHash        []string `gorm:"type:text[];column:ipfs_hash" json:"ipfs_hash"`
 	CurrentIPFSHash string   `gorm:"type:varchar(255);column:current_ipfs_hash" json:"current_ipfs_hash"`
@@ -29,6 +30,15 @@ func NewIPNSManager(db *gorm.DB) *IpnsManager {
 	return &IpnsManager{DB: db}
 }
 
+func (im *IpnsManager) FindByIPNSHash(ipnsHash string) (*IPNS, error) {
+	var entry IPNS
+	im.DB.Table("ipns").Where("ipns_hash = ?").First(&entry)
+	if entry.CreatedAt == nilTime {
+		return nil, errors.New("ipns hash does not exist")
+	}
+	return &entry, nil
+}
+
 func (im *IpnsManager) UpdateIPNSEntry(expiryDateUnix int64, ipnsHash, ipfsHash, lifetime, ttl, key string, forceUpdate bool) (*IPNS, error) {
 	var entry IPNS
 	im.DB.Where("ipns_hash = ?", ipnsHash).First(&entry)
@@ -42,6 +52,7 @@ func (im *IpnsManager) UpdateIPNSEntry(expiryDateUnix int64, ipnsHash, ipfsHash,
 	if time.Now().Unix() <= currentExpiryDate && forceUpdate == false {
 		return nil, errors.New("attempting to replace non-expired record, please wait for expiration or retry with a force upload")
 	}
+	entry.Sequence++
 	entry.IPFSHash = append(entry.IPFSHash, ipfsHash)
 	entry.CurrentIPFSHash = ipfsHash
 	entry.ExpiryDateUnix = expiryDateUnix
@@ -50,6 +61,7 @@ func (im *IpnsManager) UpdateIPNSEntry(expiryDateUnix int64, ipnsHash, ipfsHash,
 	entry.Key = key
 	// only update  changed fields
 	check := im.DB.Table("ipns").Model(&nilEntry).Updates(map[string]interface{}{
+		"sequence":          &entry.Sequence,
 		"ipfs_hash":         &entry.IPFSHash,
 		"current_ipfs_hash": &entry.CurrentIPFSHash,
 		"expiry_date_unix":  &entry.ExpiryDateUnix,
@@ -62,4 +74,13 @@ func (im *IpnsManager) UpdateIPNSEntry(expiryDateUnix int64, ipnsHash, ipfsHash,
 		return nil, check.Error
 	}
 	return &entry, nil
+}
+
+func (im *IpnsManager) CreateEntry(expiryDateUnix int64, ipnsHash, ipfsHash, lifetime, ttl, key string, forceUpdate bool) (*IPNS, error) {
+	var entry *IPNS
+	entry, err := im.FindByIPNSHash(ipnsHash)
+	if err == nil {
+		return nil, errors.New("ipns hash already exists")
+	}
+	return entry, nil
 }
