d99efcbcd71c0a4a2f68ff65473b5ca465d7ebbb
potsables <alextrottier2014@gmail.com>
2018-06-19 23:13:17 -0700

    added ipfs config to announce swarm on 192.168.0.0/16

--- scripts/ipfs_install.sh
+++ scripts/ipfs_install.sh
@@ -4,6 +4,8 @@ cd ~
 MODE="daemon"
 IPFS_PATH=/ipfs
 export IPFS_PATH=/ipfs
+sudo mkdir /ipfs
+sudo chown -R rtrade:rtrade /ipfs
 wget https://dist.ipfs.io/go-ipfs/v0.4.15/go-ipfs_v0.4.15_linux-amd64.tar.gz
 tar zxvf go-ipfs*.gz
 rm *gz
