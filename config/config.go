7474af841b490d50dd5fecef56be376f0ae468e4
potsables <alextrottier2014@gmail.com>
2018-06-21 22:59:28 -0700

    updated config, removed unused config sections

--- config/config.go
+++ config/config.go
@@ -16,10 +16,6 @@ type TemporalConfig struct {
 		Password string `json:"password"`
 	} `json:"database"`
 	API struct {
-		Admin struct {
-			Username string `json:"username"`
-			Password string `json:"password"`
-		} `json:"admin"`
 		Connection struct {
 			Certificates struct {
 				CertPath string `json:"cert_path"`
