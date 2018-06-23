41b592f8a8140cbe452c81bc0ebf44edd4b6fa87
potsables <alextrottier2014@gmail.com>
2018-06-22 22:42:19 -0700

    changes from []string to pq.StringArray

--- models/ipns.go
+++ models/ipns.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/jinzhu/gorm"
+	"github.com/lib/pq"
 )
 
 // IPNS will hold all of the IPNS entries in our system
@@ -13,11 +14,11 @@ type IPNS struct {
 	// the ipns hash, is the peer id of the peer used to sign the entry
 	IPNSHash string `gorm:"type:varchar(255);unique;column:ipns_hash" json:"ipns_hash"`
 	// List of content hashes this IPNS entry has pointed to
-	IPFSHashes      []string `gorm:"type:text[];column:ipfs_hash" json:"ipfs_hashes"`
-	CurrentIPFSHash string   `gorm:"type:varchar(255);column:current_ipfs_hash" json:"current_ipfs_hash"`
-	LifeTime        string   `gorm:"type:varchar(255)" json:"life_time"`
-	TTL             string   `gorm:"type:varchar(255)" json:"ttl"`
-	Key             string   `gorm:"type:varchar(255)" json:"key"`
+	IPFSHashes      pq.StringArray `gorm:"type:text[];column:ipfs_hash" json:"ipfs_hashes"`
+	CurrentIPFSHash string         `gorm:"type:varchar(255);column:current_ipfs_hash" json:"current_ipfs_hash"`
+	LifeTime        string         `gorm:"type:varchar(255)" json:"life_time"`
+	TTL             string         `gorm:"type:varchar(255)" json:"ttl"`
+	Key             string         `gorm:"type:varchar(255)" json:"key"`
 }
 
 type IpnsManager struct {
@@ -42,9 +43,14 @@ func (im *IpnsManager) FindByIPNSHash(ipnsHash string) (*IPNS, error) {
 func (im *IpnsManager) UpdateIPNSEntry(ipnsHash, ipfsHash, lifetime, ttl, key string) (*IPNS, error) {
 	var entry IPNS
 	im.DB.Where("ipns_hash = ?", ipnsHash).First(&entry)
-	/*if entry.CreatedAt == nilTime {
-		return nil, errors.New("ipns hash does not exist")
-	}*/
+	if entry.CreatedAt == nilTime {
+		// Create the record
+		entry, err := im.CreateEntry(ipnsHash, ipfsHash, lifetime, ttl, key)
+		if err != nil {
+			return nil, err
+		}
+		return entry, nil
+	}
 	entry.Sequence++
 	entry.IPNSHash = ipnsHash
 	entry.IPFSHashes = append(entry.IPFSHashes, ipfsHash)
@@ -53,7 +59,7 @@ func (im *IpnsManager) UpdateIPNSEntry(ipnsHash, ipfsHash, lifetime, ttl, key st
 	entry.TTL = ttl
 	entry.Key = key
 	// only update  changed fields
-	check := im.DB.Table("ip_ns").Model(&nilEntry).Updates(map[string]interface{}{
+	check := im.DB.Table("ip_ns").Model(&entry).Updates(map[string]interface{}{
 		"sequence":          &entry.Sequence,
 		"ipns_hash":         &entry.IPNSHash,
 		"ipfs_hashes":       &entry.IPFSHashes,
@@ -69,11 +75,20 @@ func (im *IpnsManager) UpdateIPNSEntry(ipnsHash, ipfsHash, lifetime, ttl, key st
 	return &entry, nil
 }
 
-func (im *IpnsManager) CreateEntry(expiryDateUnix int64, ipnsHash, ipfsHash, lifetime, ttl, key string, forceUpdate bool) (*IPNS, error) {
-	var entry *IPNS
-	entry, err := im.FindByIPNSHash(ipnsHash)
+func (im *IpnsManager) CreateEntry(ipnsHash, ipfsHash, lifetime, ttl, key string) (*IPNS, error) {
+	var entry IPNS
+	_, err := im.FindByIPNSHash(ipnsHash)
 	if err == nil {
 		return nil, errors.New("ipns hash already exists")
 	}
-	return entry, nil
+	entry.Sequence = 1
+	entry.IPNSHash = ipnsHash
+	entry.IPFSHashes = append(entry.IPFSHashes, ipfsHash)
+	entry.LifeTime = lifetime
+	entry.TTL = ttl
+	entry.Key = key
+	if check := im.DB.Create(&entry); check.Error != nil {
+		return nil, check.Error
+	}
+	return &entry, nil
 }
