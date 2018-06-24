e2cc4ebe50bf45dc53ced5c00b28ab5f0c4a61bb
potsables <alextrottier2014@gmail.com>
2018-06-24 16:30:01 -0700

    framework for gui

--- gui/gui.go
+++ gui/gui.go
@@ -10,6 +10,7 @@ import (
 const ApplicationID = "com.rtradetechnologies"
 
 func Run() {
+
 	// This is used to create our root GTK application
 	application, err := gtk.ApplicationNew(ApplicationID, glib.APPLICATION_FLAGS_NONE)
 	if err != nil {
@@ -17,36 +18,29 @@ func Run() {
 	}
 
 	application.Connect("activate", func() {
-		// create an application window attached to our main application
 		appWindow, err := gtk.ApplicationWindowNew(application)
 		if err != nil {
 			log.Fatal(err)
 		}
-		// Set the title
-		appWindow.SetTitle("Temporal GUI")
-		// Set the size
+		appWindow.SetTitle("Basic Application")
+		appWindow.Add(generateAndSetupGrid())
 		appWindow.SetDefaultSize(400, 400)
-		grid, err := gtk.GridNew()
-		if err != nil {
-			log.Fatal(err)
-		}
-		// Lets create a button
-		btnTest, err := gtk.ButtonNewWithLabel("test")
-		if err != nil {
-			log.Fatal(err)
-		}
-		btnAbout, err := gtk.ButtonNewWithLabel("about")
-		if err != nil {
-			log.Fatal(err)
-		}
-		// Add the button
-		grid.Add(btnTest)
-		// Attach the button can be used to specify where in teh grid
-		// to attach to
-		grid.Attach(btnAbout, 1, 1, 1, 1)
-		appWindow.Add(grid)
-		// Show all widgets
 		appWindow.ShowAll()
+
 	})
 	application.Run(nil)
+}
+
+func generateAndSetupGrid() *gtk.Grid {
+	grid, err := gtk.GridNew()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buttonAbout, err := gtk.ButtonNewWithLabel("About")
+	if err != nil {
+		log.Fatal(err)
+	}
+	grid.Add(buttonAbout)
+	return grid
 }
