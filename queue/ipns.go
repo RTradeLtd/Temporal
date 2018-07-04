ea3cdb6dfd156a8cdee978c81f03167bba1b5122
potsables <alextrottier2014@gmail.com>
2018-07-03 22:10:12 -0700

    more queue work

--- queue/ipns.go
+++ queue/ipns.go
@@ -12,6 +12,47 @@ import (
 	"github.com/streadway/amqp"
 )
 
+// ProcessIPNSUpdates is used to process IPNS updates, saving the data to the database
+func ProcessIPNSUpdates(msgs <-chan amqp.Delivery, db *gorm.DB) error {
+	im := models.NewIPNSManager(db)
+	//um := models.NewUserManager(db)
+	for d := range msgs {
+		ipnsUpdate := IPNSUpdate{}
+		err := json.Unmarshal(d.Body, &ipnsUpdate)
+		if err != nil {
+			//TODO handle
+			fmt.Println("error unmarshaling into ipns update struct ", err)
+			d.Ack(false)
+			continue
+		}
+		ipnsHash := ipnsUpdate.Key
+		ipfsHash := ipnsUpdate.CID
+		key := ipnsUpdate.Key
+		networkName := ipnsUpdate.NetworkName
+		lifetime, err := time.ParseDuration(ipnsUpdate.LifeTime)
+		if err != nil {
+			fmt.Println(err)
+			d.Ack(false)
+			continue
+		}
+		ttl, err := time.ParseDuration(ipnsUpdate.TTL)
+		if err != nil {
+			fmt.Println(err)
+			d.Ack(false)
+			continue
+		}
+		_, err = im.UpdateIPNSEntry(ipnsHash, ipfsHash, key, networkName, lifetime, ttl)
+		if err != nil {
+			fmt.Println(err)
+			d.Ack(false)
+			continue
+		}
+
+	}
+	return nil
+}
+
+// ProcessIPNSPublishRequests is used to process IPNS requests, saving the data to the database and publishing an entry and announcing it
 func ProcessIPNSPublishRequests(msgs <-chan amqp.Delivery, db *gorm.DB) error {
 	var ipnsUpdate IPNSUpdate
 	var resolve bool
