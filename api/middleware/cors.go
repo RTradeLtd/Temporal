e3eb7ebee1dcc350b73036d142fa90664b52f880
potsables <alextrottier2014@gmail.com>
2018-07-04 20:26:23 -0700

    attempting fix to allow credentials

--- api/middleware/cors.go
+++ api/middleware/cors.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 // https://stackoverflow.com/questions/29418478/go-gin-framework-cors
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "207.6.51.162")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
