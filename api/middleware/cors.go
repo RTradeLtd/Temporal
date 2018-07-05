71965c72d6329f821b43db0ad61f93839c4b9428
potsables <alextrottier2014@gmail.com>
2018-07-04 20:34:20 -0700

    more cors work

--- api/middleware/cors.go
+++ api/middleware/cors.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 // https://stackoverflow.com/questions/29418478/go-gin-framework-cors
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "207.6.51.162, nuts.rtradetechnologies.com, 172.218.49.115")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "207.6.51.162, nuts.rtradetechnologies.com:6767, 172.218.49.115")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
