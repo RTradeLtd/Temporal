b03f669bf4d1fdc3cca37f7205680f84cad75905
potsables <alextrottier2014@gmail.com>
2018-07-20 14:34:49 +0200

    added email tests

--- mail/mail_test.go
+++ mail/mail_test.go
@@ -1,18 +1,26 @@
 package mail_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/RTradeLtd/Temporal/config"
 	"github.com/RTradeLtd/Temporal/mail"
 )
 
+var recipient = "insertemailhere"
+
 func TestMail(t *testing.T) {
 	cfg, err := config.LoadConfig("/home/solidity/config.json")
 	if err != nil {
 		t.Fatal(err)
 	}
-	_, err = mail.GenerateMailManager(cfg)
+	mm, err := mail.GenerateMailManager(cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+	content := fmt.Sprint("<br>WowSuchEmail<br>WowSuchFormat")
+	_, err = mm.SendEmail("testEmail", content, "", "wowmuchemail", recipient)
 	if err != nil {
 		t.Fatal(err)
 	}
