c370563986f2c9acea2ac28d8dfea7880a75f449
potsables <alextrottier2014@gmail.com>
2018-07-27 22:09:22 +0200

    removed an unused function

--- queue/ipns.go
+++ queue/ipns.go
@@ -23,6 +23,7 @@ type IPNSEntry struct {
 	NetworkName string        `json:"network_name"`
 }
 
+// ProcessIPNSEntryCreationRequests is used to process IPNS entry creation requests
 func ProcessIPNSEntryCreationRequests(msgs <-chan amqp.Delivery, db *gorm.DB, cfg *config.TemporalConfig) error {
 	ipfsManager, err := rtfs.Initialize("", "")
 	err = ipfsManager.CreateKeystoreManager()
@@ -137,7 +138,7 @@ func ProcessIPNSEntryCreationRequests(msgs <-chan amqp.Delivery, db *gorm.DB, cf
 	return nil
 }
 
-// ProcessIPNSUpdates is used to process IPNS updates, saving the data to the database
+// ProcessIPNSUpdates is used to process any IPNS updates, saving them to the database
 func ProcessIPNSUpdates(msgs <-chan amqp.Delivery, db *gorm.DB) error {
 	im := models.NewIPNSManager(db)
 	//um := models.NewUserManager(db)
@@ -181,70 +182,6 @@ func ProcessIPNSUpdates(msgs <-chan amqp.Delivery, db *gorm.DB) error {
 			continue
 		}
 
-	}
-	return nil
-}
-
-// ProcessIPNSPublishRequests is used to process IPNS requests, saving the data to the database and publishing an entry and announcing it
-// THIS FUNCTION IS OUT OF DATE
-func ProcessIPNSPublishRequests(msgs <-chan amqp.Delivery, db *gorm.DB) error {
-	var ipnsUpdate IPNSUpdate
-	var resolve bool
-	var switchErr bool
-	rtfs, err := rtfs.Initialize("", "")
-	um := models.NewUserManager(db)
-	if err != nil {
-		return err
-	}
-
-	for d := range msgs {
-		err := json.Unmarshal(d.Body, &ipnsUpdate)
-		if err != nil {
-			// TODO: handle
-			fmt.Println("error unmarshaling into ipns update struct ", err)
-			d.Ack(false)
-			continue
-		}
-		contentHash := ipnsUpdate.CID
-		ttl, err := time.ParseDuration(ipnsUpdate.TTL)
-		if err != nil {
-			fmt.Println("error parsing ttl ", err)
-			//TODO: handle
-			d.Ack(false)
-			continue
-		}
-		keyName := ipnsUpdate.Key
-		lifetime, err := time.ParseDuration(ipnsUpdate.LifeTime)
-		if err != nil {
-			fmt.Println("error parsing lifetime ", err)
-			//TODO: handle
-			d.Ack(false)
-			continue
-		}
-		ethAddress := ipnsUpdate.EthAddress
-		if switchErr {
-			// TODO: handle
-			fmt.Println("errror, resolve is neither \"true\" or \"false\" ")
-			d.Ack(false)
-			continue
-		}
-		keyID, err := um.GetKeyIDByName(ethAddress, keyName)
-		if err != nil {
-			//TODO: handle
-			fmt.Println("error", err.Error())
-			d.Ack(false)
-			continue
-		}
-		resp, err := rtfs.PublishToIPNSDetails(contentHash, keyID, lifetime, ttl, resolve)
-		if err != nil {
-			// TODO: handle
-			fmt.Println("error publishing to ipns ", err)
-			d.Ack(false)
-			continue
-		}
-		fmt.Println("record published")
-		fmt.Printf("name: %s\nvalue: %s\n", resp.Name, resp.Value)
-		d.Ack(false)
 	}
 	return nil
 }
