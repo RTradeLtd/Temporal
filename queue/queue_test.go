147a0d9510f4c6f8ac0edf8f09544b52df57c8b6
potsables <alextrottier2014@gmail.com>
2018-07-28 20:00:17 -0400

    more tests

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -8,6 +8,8 @@ import (
 )
 
 var defaultConfigFile = "/home/solidity/config.json"
+var testCID = "QmPY5iMFjNZKxRbUZZC85wXb9CFgNSyzAy1LxwL62D8VGr"
+var testEthAddress = "0x7E4A2359c745A982a54653128085eAC69E446DE1"
 
 func TestQueueInitialization(t *testing.T) {
 	cfg, err := config.LoadConfig(defaultConfigFile)
@@ -64,4 +66,29 @@ func TestQueueInitialization(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestQueues(t *testing.T) {
+	cfg, err := config.LoadConfig(defaultConfigFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	qm, err := queue.Initialize(queue.IpfsPinQueue, cfg.RabbitMQ.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pin := queue.IPFSPin{
+		CID:              testCID,
+		NetworkName:      "public",
+		EthAddress:       testEthAddress,
+		HoldTimeInMonths: 10,
+	}
+
+	err = qm.PublishMessageWithExchange(pin, queue.PinExchange)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 }
