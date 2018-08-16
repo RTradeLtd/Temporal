5141ea5c7d62f494757ffc74cc686a03e9e9fd4b
potsables <alextrottier2014@gmail.com>
2018-08-15 22:06:31 -0700

    updating install scripts to work with ubuntu 18.04

--- scripts/postgresql_setup.sh
+++ scripts/postgresql_setup.sh
@@ -4,7 +4,8 @@
 # before running setup a password for the posgres linux user
 
 # setup password for posgres database user
-psql -d template1 -c "ALTER USER postgres with PASSWORD 'password123';"
+psql -c "ALTER USER postgres with PASSWORD 'password123';"
+psql -c "ALTER DATABASE temporal OWNER TO temporal;"
 createdb temporal
 
 #CREATE TABLE uploads (id intcreated_at timestamptz, updated_at timestamptz, deleted_at timestamptz, hash varchar, type varchar, hold_time_in_months int, upload_address varchar)
--- scripts/zabbix_install.sh
+++ scripts/zabbix_install.sh
@@ -1,7 +1,20 @@
 #! /bin/bash
 
+MODE="server"
+DISTRO=$(lsb_release -sc)
+
 cd ~
-wget http://repo.zabbix.com/zabbix/3.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_3.4-1+xenial_all.deb
-sudo dpkg -i zabbix-release_3.4-1+xenial_all.deb
+wget https://repo.zabbix.com/zabbix/3.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_3.4-1+bionic_all.deb
+sudo dpkg -i zabbix-release_3.4-1+bionic_all.deb
 sudo apt update -y
-sudo apt install zabbix-server-pgsql zabbix-frontend-php php-pgsql zabbix-agent  -y
+
+if [[ "$DISTRO" == "bionic" ]]; then
+    sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse" -y
+    sudo add-apt-repository ppa:ondrej/php -y
+fi
+
+if [[ "$MODE" == "server" ]]; then
+    sudo apt install zabbix-server-pgsql zabbix-frontend-php php-pgsql zabbix-agent  -y
+else
+    sudo apt install zabbix-agent -y
+fi
