46d7b25366fe128b57b66f043063c2a4a1c2ca72
potsables <alextrottier2014@gmail.com>
2018-08-15 22:10:39 -0700

    updated contribution guidelines and zabbix install script

--- scripts/zabbix_install.sh
+++ scripts/zabbix_install.sh
@@ -3,12 +3,13 @@
 MODE="server"
 DISTRO=$(lsb_release -sc)
 
-cd ~
+cd ~ || exit
+
 wget https://repo.zabbix.com/zabbix/3.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_3.4-1+bionic_all.deb
 sudo dpkg -i zabbix-release_3.4-1+bionic_all.deb
 sudo apt update -y
 
-if [[ "$DISTRO" == "bionic" ]]; then
+if [[ "$DISTRO" == "bionic" && "$MODE" == "server" ]]; then
     sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse" -y
     sudo add-apt-repository ppa:ondrej/php -y
 fi
@@ -18,3 +19,5 @@ if [[ "$MODE" == "server" ]]; then
 else
     sudo apt install zabbix-agent -y
 fi
+
+echo "Installation complete, please see https://www.zabbix.com/download?zabbix=3.4&os_distribution=ubuntu&os_version=bionic&db=PostgreSQL for additional setup instructions"
