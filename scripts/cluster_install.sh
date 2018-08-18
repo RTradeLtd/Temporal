0ac2985f0f6ebb36e812c949c4aa7ae51acbdede
potsables <alextrottier2014@gmail.com>
2018-08-18 16:30:25 -0700

    install script cleanup, removed old script

--- scripts/cluster_install.sh
+++ scripts/cluster_install.sh
@@ -1,10 +1,19 @@
 #! /bin/bash
 
-# used to install a single node ipfs cluster
+# Generate the secret for the cluster
 
+NODE="initial_peer"
+
+# if we are setting up the first node, lets generate a new cluster secret
+if [[ "$NODE" == "initial_peer" ]]; then
+    CLUSTER_SECRET=$(od  -vN 32 -An -tx1 /dev/urandom | tr -d ' \n')
+    export CLUSTER_SECRET=$(od  -vN 32 -An -tx1 /dev/urandom | tr -d ' \n')
+fi
 
 IPFS_CLUSTER_PATH=/ipfs/ipfs-cluster
 export IPFS_CLUSTER_PATH=/ipfs/ipfs-cluster
+
+# Install and initialize ipfs-cluster
 
 cd ~ || exit
 wget https://dist.ipfs.io/ipfs-cluster-service/v0.4.0/ipfs-cluster-service_v0.4.0_linux-amd64.tar.gz
