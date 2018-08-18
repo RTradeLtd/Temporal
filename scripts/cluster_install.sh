2b1ce1277c3e81335bef5c1a7e64a154561cf8f6
potsables <alextrottier2014@gmail.com>
2018-08-18 16:26:23 -0700

    added missing sudo perms

--- scripts/cluster_install.sh
+++ scripts/cluster_install.sh
@@ -12,10 +12,10 @@ tar zxvf ipfs-cluster-service*.tar.gz
 rm -- *gz
 cd ipfs-cluster-service || exit
 ./ipfs-cluster-service init
-cp ipfs-cluster-service /usr/local/bin
+sudo cp ipfs-cluster-service /usr/local/bin
 cd ~ || exit
 wget https://dist.ipfs.io/ipfs-cluster-ctl/v0.4.0/ipfs-cluster-ctl_v0.4.0_linux-amd64.tar.gz
 tar zxvf ipfs-cluster-ctl*.tar.gz
 rm -- *gz
 cd ipfs-cluster-ctl || exit
-cp ipfs-cluster-ctl /usr/local/bin
+sudo cp ipfs-cluster-ctl /usr/local/bin
