df400a8ff1cfec4ec2145f5ac5f9973deab3241a
potsables <alextrottier2014@gmail.com>
2018-08-20 19:49:01 -0700

    updated ipfs install to include private networks

--- scripts/ipfs_install.sh
+++ scripts/ipfs_install.sh
@@ -1,14 +1,41 @@
 #! /bin/bash
 
+IPFS_PATH="/ipfs"
+PRIVATE_NODE="yes"
+IPFS_DOWNLOAD_URL="https://dist.ipfs.io/go-ipfs/v0.4.17/go-ipfs_v0.4.17_linux-amd64.tar.gz"
+export IPFS_PATH="/ipfs"
+
 cd ~ || exit
 
-IPFS_PATH=/ipfs
-export IPFS_PATH=/ipfs
 sudo mkdir /ipfs
 sudo chown -R rtrade:rtrade /ipfs
-wget https://dist.ipfs.io/go-ipfs/v0.4.17/go-ipfs_v0.4.17_linux-amd64.tar.gz
+echo "[INFO] Downloading IPFS"
+wget "$IPFS_DOWNLOAD_URL"
 tar zxvf go-ipfs*.gz
-rm *gz
-cd go-ipfs
+rm -- *gz
+cd go-ipfs || exit
+echo "[INFO] Running ipfs install script"
 sudo ./install.sh
+echo "[INFO] Initializing ipfs with server and badgerds profile"
 ipfs init --profile=server,badgerds >> ~/ipfs_init_log.txt
+
+if [[ "$PRIVATE_NODE" == "yes" ]]; then
+    
+    echo "[INFO] Setting up private network"
+
+    GO=$(go -v)
+    if [[ "$GO" == "" ]]; then
+        echo "[ERROR] Please install golang first"
+        exit 1
+    fi
+
+    echo "[INFO] Installing ipfs swarm key gen"
+    go get -v github.com/Kubuxu/go-ipfs-swarm-key-gen/ipfs-swarm-key-gen
+    echo "[INFO] generating swarm key"
+    ipfs-swarm-key-gen > "$IPFS_PATH/swarm.key"
+    echo "[INFO] removing bootstrap peers"
+    ipfs bootstrap rm --all
+    echo "[INFO] setting LIBP2P_FORCE_PNET=1 please set this in your bashrc file to force private networks"
+    export LIBP2P_FORCE_PNET=1
+
+fi
