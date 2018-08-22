f6dcdaafec0876eaf4b9f0156512bcbf94e89caf
potsables <alextrottier2014@gmail.com>
2018-08-21 22:38:15 -0700

    missed a return statement, added script to generate test certs

--- api/routes_account.go
+++ api/routes_account.go
@@ -56,6 +56,7 @@ func RegisterUserAccount(c *gin.Context) {
 	username, exists := c.GetPostForm("username")
 	if !exists {
 		FailNoExistPostForm(c, "username")
+		return
 	}
 	password, exists := c.GetPostForm("password")
 	if !exists {
