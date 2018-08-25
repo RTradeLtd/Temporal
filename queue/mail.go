287a27b2c3f06897e07368cbad2be5445d685345
potsables <alextrottier2014@gmail.com>
2018-08-25 01:06:37 -0700

    added logging to mail queue

--- queue/mail.go
+++ queue/mail.go
@@ -2,10 +2,10 @@ package queue
 
 import (
 	"encoding/json"
-	"fmt"
 
 	"github.com/RTradeLtd/Temporal/config"
 	"github.com/RTradeLtd/Temporal/mail"
+	log "github.com/sirupsen/logrus"
 	"github.com/streadway/amqp"
 )
 
@@ -44,14 +44,26 @@ type EmailSend struct {
 func (qm *QueueManager) ProcessMailSends(msgs <-chan amqp.Delivery, tCfg *config.TemporalConfig) error {
 	mm, err := mail.GenerateMailManager(tCfg)
 	if err != nil {
+		qm.Logger.WithFields(log.Fields{
+			"service": qm.QueueName,
+			"error":   err.Error(),
+		}).Error("failed to generate mail manager")
 		return err
 	}
+	qm.Logger.WithFields(log.Fields{
+		"service": qm.QueueName,
+	}).Info("process email sends")
 	for d := range msgs {
-		fmt.Println("email send notification detected")
+		qm.Logger.WithFields(log.Fields{
+			"service": qm.QueueName,
+		}).Info("detected new message")
 		es := EmailSend{}
 		err = json.Unmarshal(d.Body, &es)
 		if err != nil {
-			fmt.Println("error unmarshaling", err)
+			qm.Logger.WithFields(log.Fields{
+				"service": qm.QueueName,
+				"error":   err.Error(),
+			}).Error("failed to unmarshal message")
 			d.Ack(false)
 			continue
 		}
@@ -59,8 +71,11 @@ func (qm *QueueManager) ProcessMailSends(msgs <-chan amqp.Delivery, tCfg *config
 		for _, v := range es.UserNames {
 			resp, err := mm.UserManager.FindEmailByUserName(v)
 			if err != nil {
-				//TODO: decide on how this should be handled
-				fmt.Println("error finding email address for user", err)
+				qm.Logger.WithFields(log.Fields{
+					"service": qm.QueueName,
+					"user":    v,
+					"error":   err.Error(),
+				}).Error("failed to find email by user name")
 				d.Ack(false)
 				continue
 			}
@@ -69,11 +84,17 @@ func (qm *QueueManager) ProcessMailSends(msgs <-chan amqp.Delivery, tCfg *config
 		for k, v := range emails {
 			_, err = mm.SendEmail(es.Subject, es.Content, es.ContentType, k, v)
 			if err != nil {
-				//TODO: decide on how this should be handled
-				fmt.Printf("error sending message to %s for error %s", v, err)
+				qm.Logger.WithFields(log.Fields{
+					"service": qm.QueueName,
+					"user":    k,
+					"error":   err.Error(),
+				}).Error("failed to send email")
 			}
 		}
-		fmt.Println("emails sent")
+		qm.Logger.WithFields(log.Fields{
+			"service": qm.QueueName,
+			"users":   es.UserNames,
+		}).Info("successfully sent emails")
 		d.Ack(false)
 	}
 	return nil
