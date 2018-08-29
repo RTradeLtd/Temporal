40421b5b91ee5a80a6ec66dd3b1720d9d5a7d4ac
Robert Lin <robertlin1@gmail.com>
2018-08-28 22:09:18 -0700

    Update queue_test

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -7,64 +7,43 @@ import (
 	"github.com/RTradeLtd/Temporal/queue"
 )
 
-var defaultConfigFile = "/home/solidity/config.json"
-var testCID = "QmPY5iMFjNZKxRbUZZC85wXb9CFgNSyzAy1LxwL62D8VGr"
-var testEthAddress = "0x7E4A2359c745A982a54653128085eAC69E446DE1"
+const (
+	defaultConfigFile = "/home/solidity/config.json"
+	testCID           = "QmPY5iMFjNZKxRbUZZC85wXb9CFgNSyzAy1LxwL62D8VGr"
+	testEthAddress    = "0x7E4A2359c745A982a54653128085eAC69E446DE1"
+)
 
-func TestQueueInitialization(t *testing.T) {
+func TestInitialize(t *testing.T) {
 	cfg, err := config.LoadConfig(defaultConfigFile)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	_, err = queue.Initialize(queue.DatabaseFileAddQueue, cfg.RabbitMQ.URL)
-	if err != nil {
-		t.Fatal(err)
+	type args struct {
+		queueName     string
+		connectionURL string
+		publish       bool
+		service       bool
 	}
-
-	_, err = queue.Initialize(queue.DatabasePinAddQueue, cfg.RabbitMQ.URL)
-	if err != nil {
-		t.Fatal(err)
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"DFAQ", args{queue.DatabaseFileAddQueue, cfg.RabbitMQ.URL, false, false}},
+		{"IPQ", args{queue.IpfsPinQueue, cfg.RabbitMQ.URL, false, false}},
+		{"IFQ", args{queue.IpfsFileQueue, cfg.RabbitMQ.URL, false, false}},
+		{"PPCQ", args{queue.PinPaymentConfirmationQueue, cfg.RabbitMQ.URL, false, false}},
+		{"PPSQ", args{queue.PinPaymentSubmissionQueue, cfg.RabbitMQ.URL, false, false}},
+		{"ESQ", args{queue.EmailSendQueue, cfg.RabbitMQ.URL, false, false}},
+		{"IEQ", args{queue.IpnsEntryQueue, cfg.RabbitMQ.URL, false, false}},
+		{"IPRQ", args{queue.IpfsPinRemovalQueue, cfg.RabbitMQ.URL, false, false}},
 	}
-
-	_, err = queue.Initialize(queue.IpnsUpdateQueue, cfg.RabbitMQ.URL)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	_, err = queue.Initialize(queue.IpfsPinQueue, cfg.RabbitMQ.URL)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	_, err = queue.Initialize(queue.IpfsFileQueue, cfg.RabbitMQ.URL)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	_, err = queue.Initialize(queue.PinPaymentConfirmationQueue, cfg.RabbitMQ.URL)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	_, err = queue.Initialize(queue.PinPaymentSubmissionQueue, cfg.RabbitMQ.URL)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	_, err = queue.Initialize(queue.EmailSendQueue, cfg.RabbitMQ.URL)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	_, err = queue.Initialize(queue.IpnsEntryQueue, cfg.RabbitMQ.URL)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	_, err = queue.Initialize(queue.IpfsPinRemovalQueue, cfg.RabbitMQ.URL)
-	if err != nil {
-		t.Fatal(err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := queue.Initialize(tt.args.queueName, tt.args.connectionURL, tt.args.publish, tt.args.service)
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
 	}
 }
 
@@ -74,7 +53,7 @@ func TestQueues(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	qm, err := queue.Initialize(queue.IpfsPinQueue, cfg.RabbitMQ.URL)
+	qm, err := queue.Initialize(queue.IpfsPinQueue, cfg.RabbitMQ.URL, false, false)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -82,7 +61,6 @@ func TestQueues(t *testing.T) {
 	pin := queue.IPFSPin{
 		CID:              testCID,
 		NetworkName:      "public",
-		EthAddress:       testEthAddress,
 		HoldTimeInMonths: 10,
 	}
 
@@ -90,5 +68,4 @@ func TestQueues(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
 }
