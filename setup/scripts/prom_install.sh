dd7b986f96c9ded242154b5aa54b16dc2cb5ce92
Robert Lin <robertlin1@gmail.com>
2018-09-02 18:11:18 -0700

    Move setup scripts and configs to /setup

--- setup/scripts/prom_install.sh
+++ setup/scripts/prom_install.sh
@@ -9,9 +9,9 @@ tar zxvf prometheus-*tar.gz
 rm -- *.gz
 mkdir /usr/local/bin/prometheus_server
 mv ~/prometheus-*/* /usr/local/bin/prometheus_server
-cp ~/go/src/github.com/RTradeLtd/Temporal/configs/prometheus.yml /usr/local/bin/prometheus_server
-cp ~/go/src/github.com/RTradeLtd/Temporal/configs/prometheus_server.service /etc/systemd/system
-cp ~/go/src/github.com/RTradeLtd/Temporal/scripts/prom_server_start.sh /boot_scripts/prom_server_start.sh
+cp ~/go/src/github.com/RTradeLtd/Temporal/setup/configs/prometheus.yml /usr/local/bin/prometheus_server
+cp ~/go/src/github.com/RTradeLtd/Temporal/setup/configs/prometheus_server.service /etc/systemd/system
+cp ~/go/src/github.com/RTradeLtd/Temporal/setup/scripts/prom_server_start.sh /boot_scripts/prom_server_start.sh
 chmod a+x /boot_scripts/prom_server_start.sh
 echo "[INFO] Prometheus installed, enabling service file"
 sudo systemctl enable prometheus_server.service
