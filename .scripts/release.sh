1ea4518ca60cfa672f6dabe7ee30e79dcd82ee5c
Robert Lin <robertlin1@gmail.com>
2018-09-23 00:21:21 -0700

    chore: use gox to cross-compile releases

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -1,21 +1,14 @@
 #! /bin/bash
 
-# Cross-compile Temporal using xgo, injecting appropriate tags.
+# Cross-compile Temporal using gox, injecting appropriate tags.
+go get -u github.com/mitchellh/gox
 
 RELEASE=$(git describe --tags)
-TARGETS="linux/amd64,linux/386,linux/arm,darwin/amd64,windows/amd64"
+TARGETS="linux/amd64 linux/386 linux/arm darwin/amd64 windows/amd64"
 
 mkdir -p release
 
-# dest:    output folder
-# out:     output file name
-# ldflags: set build version (git tag)
-# targets: platforms to compile for (see $TARGET)
-# deps:    for ethereum, see https://github.com/ethereum/go-ethereum/wiki/Cross-compiling-Ethereum#building-ethereum
-xgo \
-  --dest="release" \
-  --out="temporal-$(git describe --tags)" \
-  --ldflags="-X main.Version=$RELEASE" \
-  --targets="$TARGETS" \
-  --deps="https://gmplib.org/download/gmp/gmp-6.0.0a.tar.bz2" \
-  ./cmd/temporal
+gox -output="release/temporal-$(git describe --tags)-{{.OS}}-{{.Arch}}" \
+    -ldflags "-X main.Version=$RELEASE" \
+    -osarch="$TARGETS" \
+    ./cmd/temporal
