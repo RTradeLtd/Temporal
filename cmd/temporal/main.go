71234d7f952f2a724c1c155c6d1c4022471bc240
potsables <alextrottier2014@gmail.com>
2018-09-25 19:03:54 -0700

    cmd: removed deprecated payment commands

--- cmd/temporal/main.go
+++ cmd/temporal/main.go
@@ -166,44 +166,7 @@ var commands = map[string]cmd.Cmd{
 					}
 				},
 			},
-			"payment": cmd.Cmd{
-				Blurb:         "Payment queue sub commands",
-				Description:   "Used to launch the various queues that interact with our payment backend",
-				ChildRequired: true,
-				Children: map[string]cmd.Cmd{
-					"pin-confirmation": cmd.Cmd{
-						Blurb:       "Pin payment confirmation queue",
-						Description: "Listens to pin payment confirmations and stores the pins in our system",
-						Action: func(cfg config.TemporalConfig, args map[string]string) {
-							mqConnectionURL := cfg.RabbitMQ.URL
-							qm, err := queue.Initialize(queue.PinPaymentConfirmationQueue, mqConnectionURL, false, true)
-							if err != nil {
-								log.Fatal(err)
-							}
-							err = qm.ConsumeMessage("", args["dbPass"], args["dbURL"], args["dbUser"], &cfg)
-							if err != nil {
-								log.Fatal(err)
-							}
-						},
-					},
-					"pin-submission": cmd.Cmd{
-						Blurb:       "Pin payment submission queue",
-						Description: "Listen to pin payment submissions and stores the information in our database",
-						Action: func(cfg config.TemporalConfig, args map[string]string) {
-							mqConnectionURL := cfg.RabbitMQ.URL
-							qm, err := queue.Initialize(queue.PinPaymentSubmissionQueue, mqConnectionURL, false, true)
-							if err != nil {
-								log.Fatal(err)
-							}
-							err = qm.ConsumeMessage("", args["dbPass"], args["dbURL"], args["dbUser"], &cfg)
-							if err != nil {
-								log.Fatal(err)
-							}
-						},
-					},
-				},
-			},
-			"email-send": cmd.Cmd{
+			"email-send": app.Cmd{
 				Blurb:       "Email send queue",
 				Description: "Listens to requests to send emails",
 				Action: func(cfg config.TemporalConfig, args map[string]string) {
