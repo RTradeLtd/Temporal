a211e89e23881943383c42a89052e7d76f5e1120
Robert Lin <robertlin1@gmail.com>
2018-09-29 01:25:57 -0700

    deps: lock RTradeLtd/config

--- vendor/github.com/RTradeLtd/config/config.go
+++ vendor/github.com/RTradeLtd/config/config.go
@@ -1,11 +1,13 @@
 package config
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"os"
 )
 
+// LoadConfig loads a TemporalConfig from given filepath
 func LoadConfig(configPath string) (*TemporalConfig, error) {
 	var tCfg TemporalConfig
 	raw, err := ioutil.ReadFile(configPath)
@@ -19,11 +21,17 @@ func LoadConfig(configPath string) (*TemporalConfig, error) {
 	return &tCfg, nil
 }
 
+// GenerateConfig writes a empty TemporalConfig template to given filepath
 func GenerateConfig(configPath string) error {
 	template := &TemporalConfig{}
 	b, err := json.Marshal(template)
 	if err != nil {
 		return err
 	}
-	return ioutil.WriteFile(configPath, b, os.ModePerm)
+
+	var pretty bytes.Buffer
+	if err = json.Indent(&pretty, b, "", "\t"); err != nil {
+		return err
+	}
+	return ioutil.WriteFile(configPath, pretty.Bytes(), os.ModePerm)
 }
