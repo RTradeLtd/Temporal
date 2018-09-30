85ee385462c576f2a317967ba0fc5b31114bca17
potsables <alextrottier2014@gmail.com>
2018-09-30 02:05:31 -0700

    payment: update config package in test

--- models/payment_test.go
+++ models/payment_test.go
@@ -3,8 +3,8 @@ package models_test
 import (
 	"testing"
 
-	"github.com/RTradeLtd/Temporal/config"
 	"github.com/RTradeLtd/Temporal/models"
+	"github.com/RTradeLtd/config"
 )
 
 func TestPaymentManager_NewPayment(t *testing.T) {
