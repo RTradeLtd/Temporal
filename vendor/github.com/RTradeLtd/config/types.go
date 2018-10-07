08e162dc6b5cbd6480d1d29c0af24b792f445087
potsables <alextrottier2014@gmail.com>
2018-10-07 15:43:14 -0700

    tns: basic manager startup cmd

--- vendor/github.com/RTradeLtd/config/types.go
+++ vendor/github.com/RTradeLtd/config/types.go
@@ -12,6 +12,7 @@ type TemporalConfig struct {
 	Sendgrid    `json:"sendgrid,omitempty"`
 	Ethereum    `json:"ethereum,omitempty"`
 	Wallets     `json:"wallets,omitempty"`
+	TNS         `json:"tsn,omitempty"`
 }
 
 // API configures the Temporal API
@@ -126,4 +127,10 @@ type Wallets struct {
 	DASH string `json:"dash"`
 	BTC  string `json:"btc"`
 	LTC  string `json:"ltc"`
+}
+
+type TNS struct {
+	ZoneName           string `json:"zone_name"`
+	ZoneManagerKeyName string `json:"zone_manager_key_name"`
+	ZoneKeyName        string `json:"zone_key_name"`
 }
