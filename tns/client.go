110b568fb8bde1cfaf6effee58dcffb07c36b852
potsables <alextrottier2014@gmail.com>
2018-10-07 03:08:51 -0700

    tns: added test for add peer to peer store

--- tns/client.go
+++ tns/client.go
@@ -7,6 +7,8 @@ import (
 
 	ci "github.com/libp2p/go-libp2p-crypto"
 	peer "github.com/libp2p/go-libp2p-peer"
+	pstore "github.com/libp2p/go-libp2p-peerstore"
+	ma "github.com/multiformats/go-multiaddr"
 )
 
 // GenerateTNSClient is used to generate a TNS Client
@@ -43,10 +45,34 @@ func (c *Client) QueryTNS(peerID peer.ID) error {
 	return nil
 }
 
-/*
-func (c *Client) AddPeerToPeerstore() error {
+// AddPeerToPeerStore is used to add a TNS node to our peer store list
+func (c *Client) AddPeerToPeerStore(peerID peer.ID, peerAddr string) error {
+	ipfsaddr, err := ma.NewMultiaddr(peerAddr)
+	if err != nil {
+		return err
+	}
+	pid, err := ipfsaddr.ValueForProtocol(ma.P_IPFS)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	peerid, err := peer.IDB58Decode(pid)
+	if err != nil {
+		return err
+	}
+	targetPeerAddr, err := ma.NewMultiaddr(
+		fmt.Sprintf("/ipfs/%s", peer.IDB58Encode(peerid)),
+	)
+	if err != nil {
+		return err
+	}
+	targetAddr := ipfsaddr.Decapsulate(targetPeerAddr)
+	c.Host.Peerstore().AddAddr(
+		peerid, targetAddr, pstore.PermanentAddrTTL,
+	)
+	return nil
+}
 
-}*/
 // MakeHost is used to generate the libp2p connection for our TNS client
 func (c *Client) MakeHost(pk ci.PrivKey, opts *HostOpts) error {
 	host, err := makeHost(pk, opts, true)
