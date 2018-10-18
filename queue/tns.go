53128362f0fbb9b7b542a5bace4d9ca75dbfabc4
potsables <postables@rtradetechnologies.com>
2018-10-17 19:19:16 -0700

    queue: created log helpers

--- queue/tns.go
+++ queue/tns.go
@@ -10,7 +10,6 @@ import (
 	"github.com/RTradeLtd/config"
 	"github.com/jinzhu/gorm"
 	peer "github.com/libp2p/go-libp2p-peer"
-	log "github.com/sirupsen/logrus"
 	"github.com/streadway/amqp"
 )
 
@@ -18,70 +17,48 @@ import (
 func (qm *QueueManager) ProcessTNSRecordCreation(msgs <-chan amqp.Delivery, db *gorm.DB, cfg *config.TemporalConfig) error {
 	zm := models.NewZoneManager(db)
 	rm := models.NewRecordManager(db)
-	qm.Logger.WithFields(log.Fields{
-		"service": qm.Service,
-	}).Info("processing messages")
+	qm.LogInfo("processing messages")
 	// process new messages
 	for d := range msgs {
 		// message received
-		qm.Logger.WithFields(log.Fields{
-			"service": qm.Service,
-		}).Info("new message received")
+		qm.LogInfo("new message received")
 		req := RecordCreation{}
 		// unmarshal message
 		if err := json.Unmarshal(d.Body, &req); err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to unmarshal message")
+			qm.LogError(err, "failed to unmarshal message")
 			d.Ack(false)
 			continue
 		}
 		// search for zone in db
 		if _, err := zm.FindZoneByNameAndUser(req.ZoneName, req.UserName); err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("unable to find zone")
+			qm.LogError(err, "failed to search for zone")
 			d.Ack(false)
 			continue
 		}
 		// connect to ipfs
 		rtfsManager, err := rtfs.Initialize("", "")
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to initialize connection to ipfs")
+			qm.LogError(err, "failed to initialize connection to ipfs")
 			d.Ack(false)
 			continue
 		}
 		// create our keystore manager
 		if err = rtfsManager.CreateKeystoreManager(); err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to initialize keystore manager")
+			qm.LogError(err, "failed to initialize keystore meanage")
 			d.Ack(false)
 			continue
 		}
 		// get private key for record
 		recordPK, err := rtfsManager.KeystoreManager.GetPrivateKeyByName(req.RecordKeyName)
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to get record private key")
+			qm.LogError(err, "failed to get record private key")
 			d.Ack(false)
 			continue
 		}
 		// get id
 		recordPKID, err := peer.IDFromPublicKey(recordPK.GetPublic())
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to get id from public key")
+			qm.LogError(err, "failed to get record id from public key")
 			d.Ack(false)
 			continue
 		}
@@ -94,20 +71,14 @@ func (qm *QueueManager) ProcessTNSRecordCreation(msgs <-chan amqp.Delivery, db *
 		// marshal it
 		marshaled, err := json.Marshal(&r)
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to marshal tns record")
+			qm.LogError(err, "failed to marshal tns record")
 			d.Ack(false)
 			continue
 		}
 		// put to ipfs
 		resp, err := rtfsManager.Shell.DagPut(marshaled, "json", "cbor")
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to put record file to ipfs")
+			qm.LogError(err, "failed to put record in ipfs")
 			d.Ack(false)
 		}
 		// update the zone in database
@@ -115,10 +86,7 @@ func (qm *QueueManager) ProcessTNSRecordCreation(msgs <-chan amqp.Delivery, db *
 			req.ZoneName, req.RecordName, req.UserName,
 		)
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("unable to add record to zone")
+			qm.LogError(err, "failed to add record to zone in database")
 			d.Ack(false)
 			continue
 		}
@@ -126,10 +94,7 @@ func (qm *QueueManager) ProcessTNSRecordCreation(msgs <-chan amqp.Delivery, db *
 		if _, err := rm.AddRecord(
 			req.UserName, req.RecordName, req.RecordKeyName, req.ZoneName, req.MetaData,
 		); err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("unable to add record to database")
+			qm.LogError(err, "unable to add record in database")
 			d.Ack(false)
 			continue
 		}
@@ -137,57 +102,39 @@ func (qm *QueueManager) ProcessTNSRecordCreation(msgs <-chan amqp.Delivery, db *
 		if _, err := rm.UpdateLatestIPFSHash(
 			req.UserName, req.RecordName, resp,
 		); err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("unable to update ipfs hash for record")
+			qm.LogError(err, "unable to update ipfs hash for record in database")
 			d.Ack(false)
 			continue
 		}
 		zonePK, err := rtfsManager.KeystoreManager.GetPrivateKeyByName(zone.ZonePublicKeyName)
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("unable to get zone pk")
+			qm.LogError(err, "failed to get zone private key")
 			d.Ack(false)
 			continue
 		}
 		// convert private key to id
 		zonePKID, err := peer.IDFromPublicKey(zonePK.GetPublic())
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to get peer id from public key")
+			qm.LogError(err, "failed to get zone id from public key")
 			d.Ack(false)
 			continue
 		}
 		// get zone manager private key
 		zoneManagerPK, err := rtfsManager.KeystoreManager.GetPrivateKeyByName(zone.ManagerPublicKeyName)
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to get zone manager private key")
+			qm.LogError(err, "failed to get zone manager private key")
 			d.Ack(false)
 			continue
 		}
 		zomeManagerPKID, err := peer.IDFromPublicKey(zoneManagerPK.GetPublic())
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to get zone manager id from private key")
+			qm.LogError(err, "failed to get zone manager id from private key")
 			d.Ack(false)
 			continue
 		}
 		records, err := rm.FindRecordsByZone(zone.UserName, zone.Name)
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to find records")
+			qm.LogError(err, "failed to find records")
 			d.Ack(false)
 			continue
 		}
@@ -214,36 +161,25 @@ func (qm *QueueManager) ProcessTNSRecordCreation(msgs <-chan amqp.Delivery, db *
 		// marshal to bytes
 		marshaled, err = json.Marshal(&z)
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to marshaled tns zone")
+			qm.LogError(err, "failed to marshal tns zone")
 			d.Ack(false)
 			continue
 		}
 		// put to ipfs
 		resp, err = rtfsManager.Shell.DagPut(marshaled, "json", "cbor")
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to put zone file to ipfs")
+			qm.LogError(err, "failed to put zone file in ipfs")
 			d.Ack(false)
 			continue
 		}
 		// update database with has
 		zone.LatestIPFSHash = resp
 		if _, err = zm.UpdateLatestIPFSHashForZone(zone.Name, zone.UserName, resp); err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to update zone in database")
+			qm.LogError(err, "failed to update zone in database")
 			d.Ack(false)
 			continue
 		}
-		qm.Logger.WithFields(log.Fields{
-			"service": qm.Service,
-		}).Info("record added to zone")
+		qm.LogInfo("record added to ipfs and database")
 		d.Ack(false)
 	}
 	return nil
@@ -252,90 +188,62 @@ func (qm *QueueManager) ProcessTNSRecordCreation(msgs <-chan amqp.Delivery, db *
 // ProcessTNSZoneCreation is used to process new TNS zone creation requests
 func (qm *QueueManager) ProcessTNSZoneCreation(msgs <-chan amqp.Delivery, db *gorm.DB, cfg *config.TemporalConfig) error {
 	zm := models.NewZoneManager(db)
-	qm.Logger.WithFields(log.Fields{
-		"service": qm.Service,
-	}).Info("processing messages")
+	qm.LogInfo("processing messages")
 	// process messages
 	for d := range msgs {
 		// new message
-		qm.Logger.WithFields(log.Fields{
-			"service": qm.Service,
-		}).Info("new message received")
+		qm.LogInfo("new message received")
 		req := ZoneCreation{}
 		// unmarshal the message into a typed format
 		if err := json.Unmarshal(d.Body, &req); err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to unmarshal message")
+			qm.LogError(err, "failed to unmarshal message")
 			d.Ack(false)
 			continue
 		}
 		// get the zone from db
 		zone, err := zm.FindZoneByNameAndUser(req.Name, req.UserName)
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to search for zone")
+			qm.LogError(err, "failed to search for zone")
 			d.Ack(false)
 			continue
 		}
 		// connect to ipfs
 		rtfsManager, err := rtfs.Initialize("", "")
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to intiialize connection to ipfs")
+			qm.LogError(err, "failed to initialize connection to ipfs")
 			d.Ack(false)
 			continue
 		}
 		// create keystore manager
 		if err = rtfsManager.CreateKeystoreManager(); err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to initialize keystore manager")
+			qm.LogError(err, "failed to initialize keystore meanage")
 			d.Ack(false)
 			continue
 		}
 		// get zone manager private key
 		zoneManagerPK, err := rtfsManager.KeystoreManager.GetPrivateKeyByName(req.ManagerKeyName)
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to get zone manager private key")
+			qm.LogError(err, "failed to get zone manager private key")
 			d.Ack(false)
 			continue
 		}
 		// get zone private key
 		zonePK, err := rtfsManager.KeystoreManager.GetPrivateKeyByName(req.ZoneKeyName)
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to initialize keystore manager")
+			qm.LogError(err, "failed to get zone private key")
 			d.Ack(false)
 			continue
 		}
 		// convert private key to id
 		zonePKID, err := peer.IDFromPublicKey(zonePK.GetPublic())
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to get peer id from public key")
+			qm.LogError(err, "failed to get zone peer id from public key")
 			d.Ack(false)
 			continue
 		}
 		zoneManagerPKID, err := peer.IDFromPublicKey(zoneManagerPK.GetPublic())
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to get peer id from public key")
+			qm.LogError(err, "failed to get zone manager peer id from pubclic key")
 			d.Ack(false)
 			continue
 		}
@@ -350,37 +258,26 @@ func (qm *QueueManager) ProcessTNSZoneCreation(msgs <-chan amqp.Delivery, db *go
 		// marshal to bytes
 		marshaled, err := json.Marshal(&z)
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to marshaled tns zone")
+			qm.LogError(err, "failed to marshal tns zone")
 			d.Ack(false)
 			continue
 		}
 		// put to ipfs
 		resp, err := rtfsManager.Shell.DagPut(marshaled, "json", "cbor")
 		if err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to put zone file to ipfs")
+			qm.LogError(err, "failed to put zone in ipfs")
 			d.Ack(false)
 			continue
 		}
 		// update database with has
 		zone.LatestIPFSHash = resp
 		if _, err = zm.UpdateLatestIPFSHashForZone(zone.Name, zone.UserName, resp); err != nil {
-			qm.Logger.WithFields(log.Fields{
-				"service": qm.Service,
-				"error":   err.Error(),
-			}).Error("failed to update zone in database")
+			qm.LogError(err, "failed to update zone in database")
 			d.Ack(false)
 			continue
 		}
 		// success
-		qm.Logger.WithFields(log.Fields{
-			"service": qm.Service,
-		}).Info("zone published and database is updated")
+		qm.LogInfo("zone published and database updated")
 		d.Ack(false)
 		continue
 	}
