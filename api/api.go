207ecf2a9838eb4c7ab73d3dd5add3c89b52af99
potsables <postables@rtradetechnologies.com>
2018-11-03 20:36:15 +0100

    api: update keystore to only trigger in non dev

--- api/api.go
+++ api/api.go
@@ -121,11 +121,12 @@ func new(cfg *config.TemporalConfig, router *gin.Engine, debug bool, out io.Writ
 	if err != nil {
 		return nil, err
 	}
-	// create our keystore manager
-	if err = ipfsManager.CreateKeystoreManager(); err != nil {
-		return nil, err
+	if !dev {
+		// create our keystore manager
+		if err = ipfsManager.CreateKeystoreManager(); err != nil {
+			return nil, err
+		}
 	}
-
 	gc, err := grpc.NewGRPCClient(cfg, true)
 	if err != nil {
 		return nil, err
