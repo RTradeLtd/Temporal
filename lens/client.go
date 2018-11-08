a3a17298e449893cac693f95e372417f942371a1
potsables <postables@rtradetechnologies.com>
2018-11-07 18:47:52 -0800

    lens: cleanup client

--- lens/client.go
+++ lens/client.go
@@ -1,23 +1,21 @@
 package lens
 
 import (
-	"context"
-
 	pb "github.com/RTradeLtd/grpc/lens"
 	"google.golang.org/grpc"
 )
 
 // Client is a lens client used to make requests to the Lens gRPC server
 type Client struct {
-	GC *grpc.ClientConn
-	IC pb.IndexerAPIClient
+	pb.IndexerAPIClient
 }
 
 // NewClient is used to generate our lens client
 func NewClient(url string, insecure bool) (*Client, error) {
 	var (
-		gconn *grpc.ClientConn
-		err   error
+		client *Client
+		gconn  *grpc.ClientConn
+		err    error
 	)
 	if insecure {
 		gconn, err = grpc.Dial(url, grpc.WithInsecure())
@@ -25,19 +23,6 @@ func NewClient(url string, insecure bool) (*Client, error) {
 	if err != nil {
 		return nil, err
 	}
-	indexerConn := pb.NewIndexerAPIClient(gconn)
-	return &Client{
-		GC: gconn,
-		IC: indexerConn,
-	}, nil
-}
-
-// SubmitIndexRequest is used to submit an index request to lens
-func (c *Client) SubmitIndexRequest(ctx context.Context, req *pb.IndexRequest) (*pb.IndexResponse, error) {
-	return c.IC.SubmitIndexRequest(ctx, req)
-}
-
-// SubmitSearchRequest is used to submit a request to search through lens
-func (c *Client) SubmitSearchRequest(ctx context.Context, req *pb.SearchRequest) (*pb.SearchResponse, error) {
-	return c.IC.SubmitSearchRequest(ctx, req)
+	client.IndexerAPIClient = pb.NewIndexerAPIClient(gconn)
+	return client, nil
 }
