b8ec8c96cf7d72db54f77d36b3f578f12d40e32b
potsables <postables@rtradetechnologies.com>
2018-12-02 00:08:05 -0800

    api: use initialize test

--- api/api_test.go
+++ api/api_test.go
@@ -1,27 +1,17 @@
 package api
 
 import (
-	"os"
 	"testing"
-	"time"
 
 	"github.com/RTradeLtd/config"
-	"github.com/RTradeLtd/rtfs"
-	"github.com/gin-gonic/gin"
 )
 
-func Test_new(t *testing.T) {
+func Test_Initialize(t *testing.T) {
 	cfg, err := config.LoadConfig("../testenv/config.json")
 	if err != nil {
 		t.Fatal(err)
 	}
-	ipfs, err := rtfs.NewManager(cfg.IPFS.APIConnection.Host+":"+cfg.IPFS.APIConnection.Port, nil, time.Minute*5)
-	if err != nil {
+	if _, err := Initialize(cfg, true); err != nil {
 		t.Fatal(err)
 	}
-	api, err := new(cfg, gin.New(), ipfs, true, os.Stdout)
-	if err != nil {
-		t.Fatal(err)
-	}
-	api.setupRoutes()
 }
