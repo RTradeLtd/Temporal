d1ccc60d4bab6a90876b4b913c2f2e042827a0f4
potsables <postables@rtradetechnologies.com>
2018-12-05 16:46:37 -0800

    api: add database tests

--- api/v2/api_test.go
+++ api/v2/api_test.go
@@ -367,6 +367,106 @@ func Test_API_Routes_IPFS(t *testing.T) {
 	}
 }
 
+func Test_API_Routes_Database(t *testing.T) {
+	// load configuration
+	cfg, err := config.LoadConfig("../../testenv/config.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// setup connection to ipfs-node-1
+	im, err := rtfs.NewManager(
+		cfg.IPFS.APIConnection.Host+":"+cfg.IPFS.APIConnection.Port,
+		nil,
+		time.Minute*10,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// create our test api
+	testRecorder := httptest.NewRecorder()
+	_, engine := gin.CreateTestContext(testRecorder)
+	lensClient := mocks.FakeIndexerAPIClient{}
+	api, err := new(cfg, engine, &lensClient, im, false, os.Stdout)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// setup api routes
+	if err = api.setupRoutes(); err != nil {
+		t.Fatal(err)
+	}
+
+	// authenticate with the the api to get our token for testing
+	// /api/v2/auth/login
+	testRecorder = httptest.NewRecorder()
+	req := httptest.NewRequest("POST", "/api/v2/auth/login", strings.NewReader("{\n  \"username\": \"testuser\",\n  \"password\": \"admin\"\n}"))
+	req.Header.Add("Content-Type", "application/json")
+	api.r.ServeHTTP(testRecorder, req)
+	// validate the http status code
+	if testRecorder.Code != 200 {
+		t.Fatal("bad http status code from /api/v2/auth/login")
+	}
+	bodyBytes, err := ioutil.ReadAll(testRecorder.Result().Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var loginResp loginResponse
+	if err = json.Unmarshal(bodyBytes, &loginResp); err != nil {
+		t.Fatal(err)
+	}
+	// format authorization header
+	authHeader := "Bearer " + loginResp.Token
+
+	// test database global uploads
+	// /api/v2/database/uploads
+	testRecorder = httptest.NewRecorder()
+	req = httptest.NewRequest("GET", "/api/v2/database/uploads", nil)
+	req.Header.Add("Authorization", authHeader)
+	api.r.ServeHTTP(testRecorder, req)
+	if testRecorder.Code != 200 {
+		t.Fatal("bad http status code from /api/v2/database/uploads")
+	}
+	type uploadsResponse struct {
+		Code     int         `json:"code"`
+		Response interface{} `json:"response"`
+	}
+	var uploadsResp uploadsResponse
+	// unmarshal the response
+	bodyBytes, err = ioutil.ReadAll(testRecorder.Result().Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = json.Unmarshal(bodyBytes, &uploadsResp); err != nil {
+		t.Fatal(err)
+	}
+	// validate the response code
+	if uploadsResp.Code != 200 {
+		t.Fatal("bad api status code from api/v2/database/uploads")
+	}
+
+	// test database specific uploads
+	// /api/v2/database/uploads/testuser
+	testRecorder = httptest.NewRecorder()
+	req = httptest.NewRequest("GET", "/api/v2/database/uploads/testuser", nil)
+	req.Header.Add("Authorization", authHeader)
+	api.r.ServeHTTP(testRecorder, req)
+	if testRecorder.Code != 200 {
+		t.Fatal("bad http status code from /api/v2/database/uploads/testuser")
+	}
+	uploadsResp = uploadsResponse{}
+	// unmarshal the response
+	bodyBytes, err = ioutil.ReadAll(testRecorder.Result().Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = json.Unmarshal(bodyBytes, &uploadsResp); err != nil {
+		t.Fatal(err)
+	}
+	// validate the response code
+	if uploadsResp.Code != 200 {
+		t.Fatal("bad api status code from api/v2/database/uploads/testuser")
+	}
+}
+
 func Test_API_Routes_Account(t *testing.T) {
 	// load configuration
 	cfg, err := config.LoadConfig("../../testenv/config.json")
