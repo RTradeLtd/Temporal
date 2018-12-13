8e15c9e138fa1dda4e4b20a9438c2477ca382b30
potsables <postables@rtradetechnologies.com>
2018-12-13 12:22:32 -0800

    rtfscluster: cleanup some tests

--- rtfscluster/rtfs_cluster_test.go
+++ rtfscluster/rtfs_cluster_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/RTradeLtd/Temporal/rtfscluster"
+	gocid "github.com/ipfs/go-cid"
 )
 
 const (
@@ -36,6 +37,33 @@ func TestInitialize_Failure(t *testing.T) {
 		t.Fatal("expected error")
 	}
 }
+
+func TestDecodeHashString(t *testing.T) {
+	cm, err := rtfscluster.Initialize(nodeOneAPIAddr, nodePort)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	type args struct {
+		hash string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"Success", args{testPIN}, false},
+		{"Failure", args{"notahash"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if _, err := cm.DecodeHashString(tt.args.hash); (err != nil) != tt.wantErr {
+				t.Fatalf("DecodeHashString() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
 func TestParseLocalStatusAllAndSync(t *testing.T) {
 	cm, err := rtfscluster.Initialize(nodeOneAPIAddr, nodePort)
 	if err != nil {
@@ -63,8 +91,23 @@ func TestClusterPin(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if err = cm.Pin(decoded); err != nil {
-		t.Fatal(err)
+	type args struct {
+		cid gocid.Cid
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"Success", args{decoded}, false},
+		{"Failure", args{gocid.Cid{}}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := cm.Pin(tt.args.cid); (err != nil) != tt.wantErr {
+				t.Fatalf("Pin() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
 	}
 }
 
@@ -87,14 +130,28 @@ func TestGetStatusForCidLocally(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	status, err := cm.GetStatusForCidLocally(testPIN)
-	if err != nil {
-		t.Fatal(err)
+	type args struct {
+		hash string
 	}
-
-	if status == nil {
-		t.Fatal("status is nil when it shouldn't be")
+	tests := []struct {
+		name       string
+		args       args
+		wantErr    bool
+		wantStatus bool
+	}{
+		{"Success", args{testPIN}, false, true},
+		{"Failure", args{"blah"}, true, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			status, err := cm.GetStatusForCidLocally(tt.args.hash)
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("GetStatusForCidLocally() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if (status != nil) != tt.wantStatus {
+				t.Fatalf("GetStatusForCidLocally() status = %v, wantStatus %v", status, tt.wantStatus)
+			}
+		})
 	}
 }
 
@@ -103,12 +160,28 @@ func TestGetStatusForCidGlobally(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	status, err := cm.GetStatusForCidGlobally(testPIN)
-	if err != nil {
-		t.Fatal(err)
+	type args struct {
+		hash string
 	}
-	if status == nil {
-		t.Fatal("status is nil when it shouldn't be")
+	tests := []struct {
+		name       string
+		args       args
+		wantErr    bool
+		wantStatus bool
+	}{
+		{"Success", args{testPIN}, false, true},
+		{"Failure", args{"blah"}, true, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			status, err := cm.GetStatusForCidGlobally(tt.args.hash)
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("GetStatusForCidGlobally() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if (status != nil) != tt.wantStatus {
+				t.Fatalf("GetStatusForCidGlobally() status = %v, wantStatus %v", status, tt.wantStatus)
+			}
+		})
 	}
 }
 
@@ -125,12 +198,38 @@ func TestListPeers(t *testing.T) {
 		t.Fatal("no pers found")
 	}
 }
+
 func TestRemovePinFromCluster(t *testing.T) {
 	cm, err := rtfscluster.Initialize(nodeOneAPIAddr, nodePort)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if err = cm.RemovePinFromCluster(testPIN); err != nil {
-		t.Fatal(err)
+	type args struct {
+		hash string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"Success", args{testPIN}, false},
+		{"Failure", args{"blah"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := cm.RemovePinFromCluster(tt.args.hash); (err != nil) != tt.wantErr {
+				t.Fatalf("RemovePinFromCluster() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			// trigger a pin so we can run more tests
+			if !tt.wantErr {
+				decoded, err := cm.DecodeHashString(tt.args.hash)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if err := cm.Pin(decoded); err != nil {
+					t.Fatal(err)
+				}
+			}
+		})
 	}
 }
