6b000823fc4b3123b0d2c188947c58e8815cad68
potsables <postables@rtradetechnologies.com>
2018-12-15 19:36:58 -0800

    queue: add basic log test for instantiating a new queue

---
+++ queue/log_test.go
@@ -0,0 +1,44 @@
+package queue
+
+import (
+	"testing"
+
+	"github.com/RTradeLtd/config"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest/observer"
+)
+
+func TestLog_Publisher(t *testing.T) {
+	cfg, err := config.LoadConfig(testCfgPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	observer, out := observer.New(zap.InfoLevel)
+	logger := zap.New(observer).Sugar()
+	_, err = New(IpfsFileQueue, cfg.RabbitMQ.URL, true, logger)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if out.All()[out.Len()-1].Message != "channel opened" {
+		t.Fatal("failed to recover correct message")
+	}
+}
+
+func TestLog_Consumer(t *testing.T) {
+	cfg, err := config.LoadConfig(testCfgPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	observer, out := observer.New(zap.InfoLevel)
+	logger := zap.New(observer).Sugar()
+	qm, err := New(IpfsFileQueue, cfg.RabbitMQ.URL, false, logger)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if out.All()[out.Len()-1].Message != "queue declared" {
+		t.Fatal("failed to recover correct message")
+	}
+	if err = qm.Close(); err != nil {
+		t.Fatal(err)
+	}
+}
