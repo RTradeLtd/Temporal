e01c11649d019d1f2407166f06e0b09f515cbf37
potsables <postables@rtradetechnologies.com>
2019-02-01 00:35:09 -0800

    api/v2: add additional utility tests

--- api/v2/utils_test.go
+++ api/v2/utils_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/RTradeLtd/Temporal/mocks"
 	"github.com/RTradeLtd/Temporal/utils"
 	"github.com/RTradeLtd/config"
+	"github.com/RTradeLtd/database/models"
 )
 
 func TestEmailJWT(t *testing.T) {
@@ -46,5 +47,62 @@ func TestEmailJWT(t *testing.T) {
 	}
 	if err := api.verifyEmailJWTToken(tkn, randUser); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func Test_CheckAccessForPrivateNetwork(t *testing.T) {
+	// load configuration
+	cfg, err := config.LoadConfig("../../testenv/config.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	db, err := loadDatabase(cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+	um := models.NewUserManager(db)
+	if err := um.AddIPFSNetworkForUser("testuser", "mynewnetworktotestwith"); err != nil {
+		t.Fatal(err)
+	}
+	// search for a non-existent user
+	if err := CheckAccessForPrivateNetwork("notarealuseraccount", "notarealnetwork", db); err == nil {
+		t.Fatal("expected error")
+	}
+	// search for network user does not have access to
+	if err := CheckAccessForPrivateNetwork("testuser", "thisnetworkdoesnotexist", db); err == nil {
+		t.Fatal("expected error")
+	}
+	if err := CheckAccessForPrivateNetwork("testuser", "mynewnetworktotestwith", db); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test_GetIPFSEndPoint(t *testing.T) {
+	// load configuration
+	cfg, err := config.LoadConfig("../../testenv/config.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	db, err := loadDatabase(cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// setup fake mock clients
+	fakeLens := &mocks.FakeIndexerAPIClient{}
+	fakeOrch := &mocks.FakeServiceClient{}
+	fakeSigner := &mocks.FakeSignerClient{}
+
+	api, _, err := setupAPI(fakeLens, fakeOrch, fakeSigner, cfg, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	dev = !dev
+	if net := api.GetIPFSEndpoint("network"); net == "" {
+		t.Fatal("bad network url recovered")
+	}
+	dev = !dev
+	if net := api.GetIPFSEndpoint("network"); net == "" {
+		t.Fatal("bad network url recovered")
 	}
 }
