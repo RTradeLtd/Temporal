cc06add1d4d1956dd3548feb0f00492676f295b5
potsables <postables@rtradetechnologies.com>
2019-03-10 20:56:21 -0700

    utils: update clammy test to not embed eicar

--- utils/clammy_test.go
+++ utils/clammy_test.go
@@ -1,13 +1,19 @@
 package utils
 
 import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"strings"
 	"testing"
 )
 
 const (
 	defaultURL = "127.0.0.1:3310"
-	eicar      = `X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"`
+	eicarURL   = "https://gateway.temporal.cloud/ipfs/QmYwvxdRF92aW7rxB1xHE3g1rHqa88s9rCbeAhb2BNg4es"
+	eicarFile  = "./eicar.txt"
 )
 
 func TestClam(t *testing.T) {
@@ -15,6 +21,15 @@ func TestClam(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	if err := downloadEicar(); err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(eicarFile)
+	eicarBytes, err := ioutil.ReadFile(eicarFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	eicar := string(eicarBytes)
 	if err := s.Scan(strings.NewReader("hello")); err != nil {
 		t.Fatal(err)
 	}
@@ -28,4 +43,23 @@ func TestClam(t *testing.T) {
 	} else if err.Error() != "virus found" {
 		t.Fatal("failed to get correct error message")
 	}
+}
+
+func downloadEicar() error {
+	resp, err := http.Get(eicarURL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	out, err := os.Create(eicarFile)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	if copied, err := io.Copy(out, resp.Body); err != nil {
+		return err
+	} else if copied == 0 {
+		return errors.New("unknown copy error")
+	}
+	return nil
 }
