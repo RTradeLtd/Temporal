db75f85e3a21a144a28b79d0ea7a63ad479c8370
Alex Trottier <postables@rtradetechnologies.com>
2019-11-24 01:28:12 -0800

    api/v2: cleanup test

--- api/v2/routes_ens_test.go
+++ api/v2/routes_ens_test.go
@@ -1,13 +1,11 @@
 package v2
 
 import (
-	"net/http/httptest"
 	"net/url"
 	"testing"
 
 	"github.com/RTradeLtd/Temporal/mocks"
 	"github.com/RTradeLtd/config/v2"
-	"github.com/gin-gonic/gin"
 )
 
 func Test_API_Routes_ENS(t *testing.T) {
@@ -26,27 +24,33 @@ func Test_API_Routes_ENS(t *testing.T) {
 	fakeSigner := &mocks.FakeSignerClient{}
 	fakeWalletService := &mocks.FakeWalletServiceClient{}
 	// instantiate the test api
-	api, testRecorder, err := setupAPI(t, fakeLens, fakeOrch, fakeSigner, fakeWalletService, cfg, db)
+	api, _, err := setupAPI(t, fakeLens, fakeOrch, fakeSigner, fakeWalletService, cfg, db)
 	if err != nil {
 		t.Fatal(err)
 	}
-	testRecorder = httptest.NewRecorder()
-	gctx, _ := gin.CreateTestContext(testRecorder)
-	req := httptest.NewRequest("GET", "/v2/ens/claim", nil)
-	vals := url.Values{}
-	req.PostForm = vals
-	req.Header.Add("Authorization", authHeader)
-	gctx.Request = req
-	api.ClaimENSName(gctx)
 
-	testRecorder = httptest.NewRecorder()
-	gctx, _ = gin.CreateTestContext(testRecorder)
-	req = httptest.NewRequest("GET", "/v2/ens/update", nil)
-	vals = url.Values{}
-	vals.Add("content_hash", hash)
-	req.PostForm = vals
-	req.Header.Add("Authorization", authHeader)
-	gctx.Request = req
-	api.usage.ClaimENSName("testuser")
-	api.UpdateContentHash(gctx)
+	// test update content hash (fail, unclaimed)
+	var resp interface{}
+	urlValues := url.Values{}
+	urlValues.Add("content_hash", hash)
+	if err := sendRequest(
+		api, "POST", "/v2/ens/update", 200, nil, urlValues, &resp,
+	); err != nil {
+		t.Fatal(err)
+	}
+
+	// test claiming name (200)
+	if err := sendRequest(
+		api, "POST", "/v2/ens/claim", 200, nil, nil, &resp,
+	); err != nil {
+		t.Fatal(err)
+	}
+	// test updating content hash (200)
+	urlValues = url.Values{}
+	urlValues.Add("content_hash", hash)
+	if err := sendRequest(
+		api, "POST", "/v2/ens/update", 200, nil, urlValues, &resp,
+	); err != nil {
+		t.Fatal(err)
+	}
 }
