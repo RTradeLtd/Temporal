745a3794d877d693bb1117e3b777fd1d75a8bdac
Alex Trottier <postables@rtradetechnologies.com>
2019-11-24 01:37:23 -0800

    api/v2: remove unused code

--- api/v2/api.go
+++ api/v2/api.go
@@ -12,7 +12,6 @@ import (
 	"github.com/streadway/amqp"
 
 	"github.com/RTradeLtd/Temporal/rtfscluster"
-	"github.com/RTradeLtd/Temporal/utils"
 	pbLens "github.com/RTradeLtd/grpc/lensv2"
 	pbOrch "github.com/RTradeLtd/grpc/nexus"
 	pbSigner "github.com/RTradeLtd/grpc/pay"
@@ -62,7 +61,6 @@ type API struct {
 	bchWallet   pbBchWallet.WalletServiceClient
 	dc          *dash.Client
 	queues      queues
-	clam        *utils.Shell
 	service     string
 
 	version string
@@ -196,10 +194,6 @@ func new(cfg *config.TemporalConfig, router *gin.Engine, l *zap.SugaredLogger, c
 	if err != nil {
 		return nil, err
 	}
-	clam, err := utils.NewShell("")
-	if err != nil {
-		return nil, err
-	}
 	if cfg.Stripe.SecretKey == "" {
 		stripeSecretKey := os.Getenv("STRIPE_SECRET_KEY")
 		cfg.Stripe.SecretKey = stripeSecretKey
@@ -240,10 +234,9 @@ func new(cfg *config.TemporalConfig, router *gin.Engine, l *zap.SugaredLogger, c
 			eth:     qmEth,
 			bch:     qmBch,
 		},
-		zm:   models.NewZoneManager(dbm.DB),
-		rm:   models.NewRecordManager(dbm.DB),
-		nm:   models.NewHostedNetworkManager(dbm.DB),
-		clam: clam,
+		zm: models.NewZoneManager(dbm.DB),
+		rm: models.NewRecordManager(dbm.DB),
+		nm: models.NewHostedNetworkManager(dbm.DB),
 	}, nil
 }
 
