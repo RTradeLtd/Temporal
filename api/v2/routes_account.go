d73c184cbf4122c5c78c803bebb1671d4273ebac
Alex Trottier <postables@rtradetechnologies.com>
2020-01-13 15:59:41 -0800

    api/v2: cleanup user creation calls

--- api/v2/routes_account.go
+++ api/v2/routes_account.go
@@ -98,78 +98,13 @@ func (api *API) registerUserAccount(c *gin.Context) {
 	}
 	// parse html encoded strings
 	forms["password"] = html.UnescapeString(forms["password"])
-	api.l.Info("user account registration detected")
 	// create user model
-	if _, err := api.um.NewUserAccount(forms["username"], forms["password"], forms["email_address"]); err != nil {
-		switch err.Error() {
-		case eh.DuplicateEmailError:
-			api.LogError(c, err, eh.DuplicateEmailError, "email", forms["email_address"])(http.StatusBadRequest)
-			return
-		case eh.DuplicateUserNameError:
-			api.LogError(c, err, eh.DuplicateUserNameError, "username", forms["username"])(http.StatusBadRequest)
-			return
-		default:
-			api.LogError(c, err, eh.UserAccountCreationError)(http.StatusBadRequest)
-			return
-		}
-	}
-	// generate a random token to validate email
-	user, err := api.um.GenerateEmailVerificationToken(forms["username"])
-	if err != nil {
-		api.LogError(c, err, eh.EmailTokenGenerationError)(http.StatusBadRequest)
-		return
-	}
-	// generate a jwt used to trigger email validation
-	token, err := api.generateEmailJWTToken(user.UserName, user.EmailVerificationToken)
-	if err != nil {
-		api.LogError(c, err, "failed to generate email verification jwt")
-		return
-	}
-	var url string
-	// format the url the user clicks to activate email
-	if dev {
-		url = fmt.Sprintf("https://dev.api.temporal.cloud/v2/account/email/verify/%s/%s", user.UserName, token)
-	} else {
-		url = fmt.Sprintf("https://api.temporal.cloud/v2/account/email/verify/%s/%s", user.UserName, token)
-
-	}
-	// format a link tag
-	link := fmt.Sprintf("<a href=\"%s\">link</a>", url)
-	// build email message
-	es := queue.EmailSend{
-		Subject:     "TEMPORAL Email Verification",
-		Content:     fmt.Sprintf("please click this %s to activate temporal email functionality", link),
-		ContentType: "text/html",
-		UserNames:   []string{user.UserName},
-		Emails:      []string{user.EmailAddress},
-	}
-	// send email message to queue for processing
-	if err = api.queues.email.PublishMessage(es); err != nil {
-		api.LogError(c, err, eh.QueuePublishError)(http.StatusBadRequest)
-		return
-	}
-	// log
-	api.l.With("user", forms["username"]).Info("user account registered")
-	// remove hashed password from output
-	user.HashedPassword = "scrubbed"
-	// remove the verification token from output
-	user.EmailVerificationToken = "scrubbed"
-	// format a custom response that includes the user model
-	// and an additional status field
-	var status string
-	if dev {
-		status = "by continuing to use this service you agree to be bound by the following api terms and service" + devTermsAndServiceURL
-	} else {
-		status = "by continuing to use this service you agree to be bound by the following api terms and service" + prodTermsAndServiceURL
-	}
-	// return
-	Respond(c, http.StatusOK, gin.H{"response": struct {
-		*models.User
-		Status string
-	}{
-		user, status,
-	},
-	})
+	_, err := api.um.NewUserAccount(
+		forms["username"],
+		forms["password"],
+		forms["email_address"],
+	)
+	api.handleUserCreate(c, forms, err)
 }
 
 // CreateIPFSKey is used to create an IPFS key
--- api/v2/routes_organizations.go
+++ api/v2/routes_organizations.go
@@ -3,17 +3,17 @@ package v2
 import (
 	"bytes"
 	"errors"
-	"fmt"
+	"html"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/RTradeLtd/Temporal/eh"
-	"github.com/RTradeLtd/Temporal/queue"
 	"github.com/RTradeLtd/database/v2/models"
 	gpaginator "github.com/RTradeLtd/gpaginator"
 	"github.com/gin-gonic/gin"
 	"github.com/jszwec/csvutil"
-	"html"
-	"net/http"
-	"strconv"
-	"time"
 )
 
 // creates a new organization
@@ -143,6 +143,13 @@ func (api *API) registerOrgUser(c *gin.Context) {
 		FailWithMissingField(c, missingField)
 		return
 	}
+	// prevent people from registering usernames that contain an `@` sign
+	// this prevents griefing by prevent user sign-ins by using a username
+	// that is based off an email address
+	if strings.ContainsRune(forms["username"], '@') {
+		Fail(c, errors.New("usernames cant contain @ sign"))
+		return
+	}
 	if _, ok := api.validateOrgOwner(c, forms["organization_name"], username); !ok {
 		return
 	}
@@ -150,115 +157,13 @@ func (api *API) registerOrgUser(c *gin.Context) {
 	forms["password"] = html.UnescapeString(forms["password"])
 	// create the org user. this process is similar to regular
 	// user registration, so we handle the errors in the same way
-	if _, err := api.orgs.RegisterOrgUser(
+	_, err = api.orgs.RegisterOrgUser(
 		forms["organization_name"],
 		forms["username"],
 		forms["password"],
 		forms["email_address"],
-	); err != nil {
-		switch err.Error() {
-		case eh.DuplicateEmailError:
-			api.LogError(
-				c,
-				err,
-				eh.DuplicateEmailError,
-				"email",
-				forms["email_address"])(http.StatusBadRequest)
-			return
-		case eh.DuplicateUserNameError:
-			api.LogError(
-				c,
-				err,
-				eh.DuplicateUserNameError,
-				"username",
-				forms["username"])(http.StatusBadRequest)
-			return
-		default:
-			api.LogError(
-				c,
-				err,
-				eh.UserAccountCreationError)(http.StatusBadRequest)
-			return
-		}
-	}
-	// generate a random token to validate email
-	user, err := api.um.GenerateEmailVerificationToken(forms["username"])
-	if err != nil {
-		api.LogError(c, err, eh.EmailTokenGenerationError)(http.StatusBadRequest)
-		return
-	}
-	// generate a jwt used to trigger email validation
-	token, err := api.generateEmailJWTToken(user.UserName, user.EmailVerificationToken)
-	if err != nil {
-		api.LogError(c, err, "failed to generate email verification jwt")
-		return
-	}
-	var url string
-	// format the url the user clicks to activate email
-	if dev {
-		url = fmt.Sprintf(
-			"https://dev.api.temporal.cloud/v2/account/email/verify/%s/%s",
-			user.UserName, token,
-		)
-	} else {
-		url = fmt.Sprintf(
-			"https://api.temporal.cloud/v2/account/email/verify/%s/%s",
-			user.UserName, token,
-		)
-
-	}
-	// format a link tag
-	link := fmt.Sprintf("<a href=\"%s\">link</a>", url)
-	emailSubject := fmt.Sprintf(
-		"%s Temporal Email Verification", forms["organization_name"],
 	)
-	// build email message
-	es := queue.EmailSend{
-		Subject: emailSubject,
-		Content: fmt.Sprintf(
-			"please click this %s to activate temporal email functionality", link,
-		),
-		ContentType: "text/html",
-		UserNames:   []string{user.UserName},
-		Emails:      []string{user.EmailAddress},
-	}
-	// send email message to queue for processing
-	if err = api.queues.email.PublishMessage(es); err != nil {
-		api.LogError(c, err, eh.QueuePublishError)(http.StatusBadRequest)
-		return
-	}
-	// log
-	api.l.With(
-		"user", forms["username"],
-		"organization", forms["organization_name"],
-		"organization.owner", username,
-	).Info("organization user account registered")
-	// remove hashed password from output
-	user.HashedPassword = "scrubbed"
-	// remove the verification token from output
-	user.EmailVerificationToken = "scrubbed"
-	// format a custom response that includes the user model
-	// and an additional status field
-	var status string
-	if dev {
-		status = fmt.Sprintf(
-			"by continuing to use this service you agree to be bound by the following api terms and service %s",
-			devTermsAndServiceURL,
-		)
-	} else {
-		status = fmt.Sprintf(
-			"by continuing to use this service you agree to be bound by the following api terms and service %s",
-			prodTermsAndServiceURL,
-		)
-	}
-	// return
-	Respond(c, http.StatusOK, gin.H{"response": struct {
-		*models.User
-		Status string
-	}{
-		user, status,
-	},
-	})
+	api.handleUserCreate(c, forms, err)
 }
 
 // getOrgUserUploads allows returning uploads for organization users
