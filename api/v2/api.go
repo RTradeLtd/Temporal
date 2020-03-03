3f6f811e141acb7446d530950c12de122418b612
postables <postables@rtradetechnologies.com>
2020-03-02 16:55:23 -0800

    api/v2: change search method

--- api/v2/api.go
+++ api/v2/api.go
@@ -614,8 +614,8 @@ func (api *API) setupRoutes(debug bool) error {
 	database := v2.Group("/database", authware...)
 	{
 		database.GET("/uploads", api.getUploadsForUser)
-		database.GET("/uploads/search", api.searchUploadsForUser)
 		database.GET("/uploads/encrypted", api.getEncryptedUploadsForUser)
+		database.POST("/uploads/search", api.searchUploadsForUser)
 	}
 
 	// frontend
